0fd1ae4bd742ce083a4c1ba192624da23c012e71
Estarly Olivar <ztrly2022@gmail.com>
2023-01-09 18:08:03 -0400

    reporte view 1 (details de estado finaciero)

--- resources/views/back/agents/reports/financial-state_view1.blade.php
+++ resources/views/back/agents/reports/financial-state_view1.blade.php
@@ -38,8 +38,41 @@
 @section('scripts')
     <script>
         $(function () {
-            let agents = new Agents();
-            agents.financialStateDetails({{ $user }});
+            {{--let agents = new Agents();--}}
+            {{--agents.financialStateDetails({{ $user }});--}}
         });
+        $(function (user = null) {
+            let picker = initLitepickerEndToday();
+            let $table = $('#financial-state-table');
+            let $button = $('#update');
+            let api;
+            if (user == null) {
+                $('#financial-state-tab').on('show.bs.tab', function () {
+                    $table.children().remove();
+                    user = $('.user').val();
+                });
+            }
+
+            $button.click(function () {
+                $button.button('loading');
+                let startDate = moment(picker.getStartDate()).format('YYYY-MM-DD');
+                let endDate = moment(picker.getEndDate()).format('YYYY-MM-DD');
+
+                $.ajax({
+                    url: `${$table.data('route')}/${user}/${startDate}/${endDate}`,
+                    type: 'get',
+                    dataType: 'json'
+
+                }).done(function (json) {
+                    $table.html(json.data.table);
+
+                }).fail(function (json) {
+                    swalError(json);
+
+                }).always(function () {
+                    $button.button('reset');
+                });
+            });
+        })
     </script>
 @endsection
