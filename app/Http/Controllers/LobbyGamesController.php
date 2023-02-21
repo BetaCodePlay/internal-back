6a56889bbc8d638ba8a7bc2b10aed19351bc501a
unknown <geneyadrian1@gmail.com>
2023-02-21 15:38:59 -0400

    modificando

--- app/Http/Controllers/LobbyGamesController.php
+++ app/Http/Controllers/LobbyGamesController.php
@@ -117,26 +117,28 @@ class LobbyGamesController extends Controller
     {
         \Log::notice(__METHOD__, ['request' => $request]);
         try {
-            if (!is_null($request->change_provider) && !is_null($request->route) && !is_null($request->games)) {
+            if (!is_null($request->change_provider)) {
                 $provider = $request->change_provider;
-                $route = $request->route;
-                $game = $request->games;
-                $order = $request->order;
-                $image = $request->image;
-                $items = Configurations::getMenu();
-                $category = 1;
-                $whitelabel = Configurations::getWhitelabel();
-                $games = $this->lobbyGamesRepo->getGamesWhitelabel($whitelabel, $category, $provider, $route, $order, $game, $image);
-                $this->lobbyGamesCollection->formatAll($games, $items, $order, $request->image);
-                $data = [
-                    'games' => $games
-                ];
-            } else {
-                $data = [
-                    'games' => []
-                ];
             }
-
+            if (!is_null($request->route)) {
+                $route = $request->route;
+            }
+            if (!is_null($request->games)) {
+                $game = $request->games;
+            }
+            $provider = $request->change_provider;
+            $route = $request->route;
+            $game = $request->games;
+            $order = $request->order;
+            $image = $request->image;
+            $items = Configurations::getMenu();
+            $category = 1;
+            $whitelabel = Configurations::getWhitelabel();
+            $games = $this->lobbyGamesRepo->getGamesWhitelabel($whitelabel, $category, $provider, $route, $order, $game, $image);
+           /*$this->lobbyGamesCollection->formatAll($games, $items, $order, $request->image);*/
+            $data = [
+                'games' => $games
+            ];
             return Utils::successResponse($data);
 
         } catch (\Exception $ex) {
@@ -162,7 +164,6 @@ class LobbyGamesController extends Controller
             $data['image'] = $image;
             $data['providers'] = $provider;
             $data['games'] = $games;
-            \Log::info(__METHOD__, ['$data' => $data['games']]);
             $data['title'] = _i('Create lobby');
             return view('back.lobby-games.games.create', $data);
         } catch (\Exception $ex) {
@@ -315,7 +316,7 @@ class LobbyGamesController extends Controller
                                     'title' => _i('Updated game'),
                                     'message' => _i('The game was updated successfully'),
                                     'close' => _i('Close'),
-                                    'route' => route('dot-suite.lobby-games.games.create')
+                                    'route' => route('lobby-games.games.create')
                                 ];
                                 return Utils::successResponse($data);
                             }
@@ -358,7 +359,7 @@ class LobbyGamesController extends Controller
                                     'title' => _i('Updated game'),
                                     'message' => _i('The game was updated successfully'),
                                     'close' => _i('Close'),
-                                    'route' => route('dot-suite.lobby-games.games.create')
+                                    'route' => route('lobby-games.games.create')
                                 ];
                                 return Utils::successResponse($data);
                             }
