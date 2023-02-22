f6ed308084dc52e7b18d2b07188db8053a8206ef
Estarly Olivar <ztrly2022@gmail.com>
2023-02-22 13:51:13 -0400

    buscador en el dashboard de agente con support

--- resources/views/back/agents/index.blade.php
+++ resources/views/back/agents/index.blade.php
@@ -177,11 +177,11 @@
                     </div>
                     <div class="row">
                         <div class="col-12 col-md-8 g-py-5 g-pa-5">
-                            <div class=" d-md-none">
+                            @if(!in_array(\Dotworkers\Security\Enums\Roles::$admin_Beet_sweet, session('roles')))
                                 <select name="agent_id_search" id="agent_id_search"  class="form-control select2 agent_id_search" data-route="{{ route('agents.search-username')}}" data-select="{{ route('agents.find-user') }}">
                                     <option></option>
                                 </select>
-                            </div>
+                            @endif
                         </div>
                         <div class="col-6 col-md-2 g-py-5">
                             <a href="#add-users-modal" data-toggle="modal" class="btn u-btn-3d u-btn-primary btn-block" id="new-user">
