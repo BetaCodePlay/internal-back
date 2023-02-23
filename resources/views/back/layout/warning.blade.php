3ba5c3f0e9f4d2f7d23615b577c3e1b9f2986aec
Clandor <carlosmejias1000@gmail.com>
2023-02-23 19:24:04 -0400

    warning

--- resources/views/back/layout/warning.blade.php
+++ resources/views/back/layout/warning.blade.php
@@ -3,8 +3,8 @@
         <div class="warning-container">
             <div class="warning-title">ATENCION NOTICIAS</div>
             <div class="warning-general-template-ex">
-                <div class="msg">La página prohíbe determinantemente decirles a los usuarios que tienen límites sus apuestas, evite sanciones.</div>
-                <div class="msg">La página prohíbe determinantemente decirles a los usuarios que tienen límites sus apuestas, evite sanciones.</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
             </div>
         </div>
     </div>
