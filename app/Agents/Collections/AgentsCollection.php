053fc726dccdcac89b19717657efb78fbf5195d7
Estarly Olivar <ztrly2022@gmail.com>
2023-03-30 18:03:05 -0400

    total in report transactions dentro de estadistica

--- app/Agents/Collections/AgentsCollection.php
+++ app/Agents/Collections/AgentsCollection.php
@@ -2766,12 +2766,12 @@ class AgentsCollection
             $transaction->debit = 0;
             if($transaction->transaction_type_id == TransactionTypes::$debit){
                 $transaction->debit = $transaction->amount;
-               // $totalDebit += $transaction->amount;
+                $totalDebit = $totalDebit +$transaction->amount;
             }
             $transaction->credit = 0;
             if($transaction->transaction_type_id == TransactionTypes::$credit){
                 $transaction->credit = $transaction->amount;
-                //$totalCredit += $transaction->amount;
+                $totalCredit = $totalCredit+$transaction->amount;
             }
             Log::notice('$transaction',[$transaction]);
             if (isset($transaction->data->balance)) {
