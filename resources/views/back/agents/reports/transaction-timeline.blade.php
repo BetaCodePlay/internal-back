25327251778aa298d37eb6e672d79a5550abc3bd
Jhonattan <jhonattanbullones@gmail.com>
2023-04-13 14:52:28 -0400

    fix

--- resources/views/back/agents/reports/transaction-timeline.blade.php
+++ resources/views/back/agents/reports/transaction-timeline.blade.php
@@ -1,7 +1,7 @@
 @extends('back.template')
 @section('styles')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.bootstrap.min.css">
-
+{{--
 {{-- <style>
     table.display {
         table-layout: fixed; /* Establecer el diseño de la tabla en fijo */
@@ -12,7 +12,7 @@
         overflow: auto; /* Ocultar el contenido que se desborda */
         text-overflow: ellipsis; /* Mostrar puntos suspensivos para el contenido que se desborda */
     }
-</style> --}}
+</style> --}} --}}
 @endsection
 @section('content')
 
