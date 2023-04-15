0bf14babe49405e7af62248c03b6464a269c732f
Orlando Bravo <jbravocadevilla@gmail.com>
2023-04-14 20:03:53 -0400

    test de responsive

--- webpack/custom-styles.mix.js
+++ webpack/custom-styles.mix.js
@@ -3,7 +3,6 @@ const mix = require('laravel-mix');
 mix.styles([
         'resources/assets/back/css/unify-admin.css',
         'resources/assets/back/css/admin-icons.css',
-        'resources/assets/back/css/custom.css',
-        'resources/assets/back/css/datatable-responsive.css',
+        'resources/assets/back/css/custom.css'
     ],
     'public/back/css/custom.min.css');
