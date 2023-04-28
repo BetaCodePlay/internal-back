37c2c18b70917c7d7cf5ef83ecdf91dfafbd277f
Estarly Olivar <ztrly2022@gmail.com>
2023-04-28 13:42:14 -0400

    proveedor 16-25

--- app/Core/Repositories/TransactionsRepo.php
+++ app/Core/Repositories/TransactionsRepo.php
@@ -449,7 +449,7 @@ class TransactionsRepo
     public function getCashFlowTransactionsNew($username, $agents, $whitelabel, $currency, $startDate, $endDate)
     {
         Log::info('getCashFlowTransactionsNew',[$username, $agents, $whitelabel, $currency, $startDate, $endDate]);
-        $providersArray = [Providers::$agents,Providers::$agents_users];
+        $providersArray = [Providers::$agents,Providers::$agents_users,Providers::$dotworkers,Providers::$manual_adjustments];
         $result = DB::SELECT("
                              SELECT u.id,
                                u.username,
