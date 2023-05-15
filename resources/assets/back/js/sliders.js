c70f452d2ace865241c77a22ce9a4f2eaed0664e
genesis <genesis.perez@dotworkers.com>
2023-05-15 12:31:57 -0400

    metodo store sliders

--- resources/assets/back/js/sliders.js
+++ resources/assets/back/js/sliders.js
@@ -120,7 +120,6 @@ class Sliders {
 
             }).done(function (json) {
                 $('#file').val(json.data.file);
-                $('#archive').val(json.data.file);
                 swalSuccessWithButton(json);
 
             }).fail(function (json) {
--- resources/views/back/sliders/create.blade.php
+++ resources/views/back/sliders/create.blade.php
@@ -55,7 +55,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="front">{{ _i('Image') }}</label>
-                                <input type="archive" name="front" id="front" class="opacity-0">
+                                <input type="file" name="front" id="front" class="opacity-0">
                             </div>
                         </div>
                     </div>
