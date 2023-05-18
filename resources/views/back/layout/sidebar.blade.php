6e4dc303818773a644a92e24dc9e1bbea63c13b3
Clandor <carlosmejias1000@gmail.com>
2023-05-18 11:00:28 -0400

    telegram

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -2,4 +2,10 @@
     <ul id="sideNavMenu" class="u-sidebar-navigation-v1-menu u-side-nav--top-level-menu g-min-height-100vh mb-0">
         {!! \Core::buildMenu() !!}
     </ul>
+
+    @if(auth()->user()->username === 'wolf')
+        <div class="col-social-navigation">
+            <a href="https://t.me/graficascasino" target="_blank"><i class="fa fa-telegram"></i></a>
+        </div>
+    @endif
 </div>
