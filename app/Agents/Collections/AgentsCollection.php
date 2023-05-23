3bb2bfdd3295858e6680d7e1660f50d53f874d6f
mayinis <hachhe.dev@gmail.com>
2023-05-23 09:03:30 -0400

    ajustes para nuevos cambios - excluir maker

--- app/Agents/Collections/AgentsCollection.php
+++ app/Agents/Collections/AgentsCollection.php
@@ -3577,7 +3577,7 @@ class AgentsCollection
             }
             
             if (is_null($category)) {
-                $categories = $gamesRepo->getCategories();
+                $categories = $gamesRepo->getCategoriesByMaker($maker);
                 foreach ($categories as $categoryElement) {
                     $excludedAgent = $this->getExcludedAgent($agentsRepo, $agent->user_id, $currency, $categoryElement->category, $whitelabel);
                     $makersExclude = isset($excludedAgent->makers) ? json_decode($excludedAgent->makers) : [];
@@ -3707,7 +3707,7 @@ class AgentsCollection
             }
             
             if (is_null($category)) {
-                $categories = $gamesRepo->getCategories();
+                $categories = $gamesRepo->getCategoriesByMaker($maker);
                 foreach ($categories as $categoryElement) {
                     $excludedAgent = $this->getExcludedAgent($agentsRepo, $agent->user_id, $currency, $categoryElement->category, $whitelabel);
                     $makersExclude = isset($excludedAgent->makers) ? json_decode($excludedAgent->makers) : [];
