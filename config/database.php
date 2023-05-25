3995a1638315e191e8a64d20a71607ece3790305
Jhonattan <jhonattanbullones@gmail.com>
2023-05-25 04:31:18 -0400

    test

--- config/database.php
+++ config/database.php
@@ -77,21 +77,6 @@ return [
             'search_path' => env('DB_SCHEMA'),
             'sslmode' => 'prefer',
         ],
-        'public_schema' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
         'replica' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
