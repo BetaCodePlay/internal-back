c1c0f7dc4045234af20e93e01fe7811657aa2557
genesis <genesis.perez@dotworkers.com>
2023-05-26 12:50:28 -0400

    secciones en lobbys

--- app/Core/helpers.php
+++ app/Core/helpers.php
@@ -2722,7 +2722,7 @@ if (!function_exists('menu')) {
                 'icon' => 'hs-admin-image',
                 'permission' => Permissions::$section_images_menu,
                 'submenu' => [
-                    'LogoFavicon' => [
+                    'LobbySections' => [
                         'text' => _i(''),
                         'level_class' => 'top',
                         'route' => null,
