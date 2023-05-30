3d26a5720e084d28151a119ea3b82b14f6d6394b
Clandor <carlosmejias1000@gmail.com>
2023-05-30 13:24:16 -0400

    footer

--- resources/views/back/layout/footer.blade.php
+++ resources/views/back/layout/footer.blade.php
@@ -67,10 +67,11 @@
             </div>
 
             <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Dropdown button
                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+                <div class="dropdown-menu">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
                     <a class="dropdown-item" href="#">Something else here</a>
