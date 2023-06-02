6e615f1332aa6f5016da4fd1172b2375c9fc0e30
Mayinis <hachhe.dev@gmail.com>
2023-06-02 11:38:48 -0400

    betpay client

--- app/Core/helpers.php
+++ app/Core/helpers.php
@@ -6387,7 +6387,7 @@ if (!function_exists('menu')) {
                 'route' => null,
                 'params' => [],
                 'icon' => 'hs-admin-settings',
-                'permission' => Permissions::$manage_betpay_menu,
+                //'permission' => Permissions::$manage_betpay_menu,
                 'submenu' => [
 
                     'ClientBetPay' => [
