1bfafdff0760018c3d4de9c2cb71a2660a475e8a
Clandor <carlosmejias1000@gmail.com>
2023-06-07 08:13:34 -0400

    warning

--- resources/views/back/layout/warning.blade.php
+++ resources/views/back/layout/warning.blade.php
@@ -2,14 +2,14 @@
     <div class="container">
         <div class="warning-container">
             <div class="warning-title">ATENCION NOTICIAS</div>
-            {{--<div class="warning-general-template-ex">
-                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
-                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
-            </div>--}}
             <div class="warning-general-template-ex">
-                <div class="msg">Miércoles 07 de Junio desde las 8PM ( Hora Argentina ) realizaremos tareas de mantenimiento ( tiempo estimado 30 minutos ).</div>
-                <div class="msg">Miércoles 07 de Junio desde las 8PM ( Hora Argentina ) realizaremos tareas de mantenimiento ( tiempo estimado 30 minutos ).</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
             </div>
+            {{--<div class="warning-general-template-ex">
+                <div class="msg">Miércoles 07 de Junio desde las 8PM ( Hora Argentina ) realizaremos tareas de mantenimiento ( tiempo estimado 30 minutos ).</div>
+                <div class="msg">Miércoles 07 de Junio desde las 8PM ( Hora Argentina ) realizaremos tareas de mantenimiento ( tiempo estimado 30 minutos ).</div>
+            </div>--}}
         </div>
     </div>
 </div>
