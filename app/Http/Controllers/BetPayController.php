241643be51af3f86a9bc6159258ec8a7a0cee055
Mayinis <hachhe.dev@gmail.com>
2023-06-20 15:38:26 -0400

    cambios para agregar cuentas betpay

--- app/Http/Controllers/BetPayController.php
+++ app/Http/Controllers/BetPayController.php
@@ -1767,6 +1767,7 @@ class BetPayController extends Controller
             'currency' => 'required',
             'payments' => 'required',
         ]);
+        \Log::debug([$request->all]);
         $rules = $this->getRulesClientAccountData($request->payments);
         $this->validate($request, $rules);
     
