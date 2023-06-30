79e9dca2789d63cf826dc40919d1a69d91c15070
genesis <genesisperez09052018@gmail.com>
2023-06-29 21:50:09 -0400

    modal

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -27,7 +27,6 @@
 </head>
 <body class="currency-theme-{{ session('currency') }}">
 @include('back.layout.header')
-@include('back.users.modals.reset-email')
 <main class="container-fluid px-0 g-pt-65">
     <div class="row no-gutters g-pos-rel g-overflow-x-hidden">
         @include('back.layout.sidebar')
@@ -41,6 +40,7 @@
 
             <div class="g-pa-20">
                 @yield('content')
+                @include('back.users.modals.reset-email')
             </div>
             @include('back.layout.footer')
         </div>
