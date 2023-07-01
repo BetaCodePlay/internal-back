648a01a6d43099607037f97194a26953dcecdd33
genesis <genesisperez09052018@gmail.com>
2023-06-30 23:29:51 -0400

    log

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2061,7 +2061,7 @@ class UsersController extends Controller
             $this->usersRepo->update($user, $userData);
             $whitelabelId = Configurations::getWhitelabel();
             $emailConfiguration = Configurations::getEmailContents($whitelabelId, EmailTypes::$validate_email);
-            Mail::to($tokenUser)->send(new Validate($whitelabelId, $url, $username, $emailConfiguration, EmailTypes::$validate_email));
+            Mail::to($users)->send(new Validate($whitelabelId, $url, $username, $emailConfiguration, EmailTypes::$validate_email));
 
             $data = [
                 'title' => _i('Email reset'),
