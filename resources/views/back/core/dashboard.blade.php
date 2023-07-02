3fa426efdbad7feb2c23b8fef877211408fb660f
genesis <genesisperez09052018@gmail.com>
2023-07-01 23:40:41 -0400

    logs

--- resources/views/back/core/dashboard.blade.php
+++ resources/views/back/core/dashboard.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     @can('access', [\Dotworkers\Security\Enums\Permissions::$dashboard_widgets])
+    @include('back.users.modals.reset-email')
         <div class="row">
             <div class="col-sm-6 col-lg-6 col-xl g-mb-30">
                 <div class="card h-100 g-brd-gray-light-v7 g-rounded-3">
