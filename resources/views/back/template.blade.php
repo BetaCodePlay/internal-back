e7e642d8da66c57c9b16c4ededbdd6c9a31f85f9
genesis <genesisperez09052018@gmail.com>
2023-07-03 10:57:56 -0400

    controlador

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -41,7 +41,7 @@
             <div class="g-pa-20">
                 @yield('content')
                 @if(!empty($action) && $action == '10')
-                   @if($agent == 0)
+                   @if($agent == 1)
                        @include('back.users.modals.reset-email')
                    @endif   
                 @endif
