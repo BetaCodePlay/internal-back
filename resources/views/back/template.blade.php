0587e018c07a14e483a585cdc9444e6cfa443bfb
genesis <genesisperez09052018@gmail.com>
2023-07-04 19:35:40 -0400

    variable

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -41,7 +41,7 @@
             <div class="g-pa-20">
                 @yield('content')
                 @if(!empty($action) && $action == \App\Users\Enums\ActionUser::$update_email)
-                   @if($agent == 1)
+                   @if($iagent == 1)
                        @include('back.users.modals.reset-email')
                    @endif
                 @endif
