2ee20503e97a44dfa7db4a27f1e09e33f392fe20
Jhonattan <jhonattanbullones@gmail.com>
2023-07-04 00:42:57 -0400

    avances

--- resources/views/auth/modals/change-password.blade.php
+++ resources/views/auth/modals/change-password.blade.php
@@ -54,18 +54,3 @@
     </div>
 </div>
 
-<div class="col-12 col-sm-6">
-    <label for="password">{{ _i('Password') }}</label>
-    <div class="input-group">
-        <input type="text" class="form-control" name="password">
-        <div class="input-group-append">
-            <button
-                class="btn u-input-btn--v1 g-width-40 u-btn-primary g-rounded-right-4 u-btn-3d refresh-password"
-                type="button">
-                <i class="hs-admin-reload g-absolute-centered g-font-size-16 g-color-white"></i>
-            </button>
-        </div>
-    </div>
-    <small
-        class="form-text text-muted">{{ _i('Minimum 8 characters, 1 letter and 1 number') }}</small>
-</div>
