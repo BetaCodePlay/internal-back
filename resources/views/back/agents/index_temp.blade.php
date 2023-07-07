8f2f4f7aa7c6bc4278d00518a7034e5d72245e0c
Clandor <carlosmejias1000@gmail.com>
2023-07-07 19:58:20 -0400

    agents

--- resources/views/back/agents/index_temp.blade.php
+++ resources/views/back/agents/index_temp.blade.php
@@ -1176,9 +1176,9 @@
 
                 $.each(users, function (index, value) {
                     if (index + 1 === users.length) {
-                        userHtmlTempMini = userHtmlTempMini + '<li class="jstree-node init_agent jstree-closed jstree-last"><i class="jstree-icon jstree-ocl" data-idtreepro="' + value.id + '"></i><a class="jstree-anchor" href="javascript:void(0)"><i class="jstree-icon jstree-themeicon fa fa-star jstree-themeicon-custom" role="presentation"></i>' + value.username + '</a></li>';
+                        userHtmlTempMini = userHtmlTempMini + '<li class="jstree-node init_agent jstree-closed jstree-last"><i class="jstree-icon jstree-ocl" data-idtreepro="' + value.id + '" role="'+ value.owner_id +'"></i><a class="jstree-anchor" href="javascript:void(0)"><i class="jstree-icon jstree-themeicon fa fa-star jstree-themeicon-custom" role="presentation"></i>' + value.username + '</a></li>';
                     } else {
-                        userHtmlTempMini = userHtmlTempMini + '<li class="jstree-node init_agent jstree-closed"><i class="jstree-icon jstree-ocl" data-idtreepro="' + value.id + '"></i><a class="jstree-anchor" href="javascript:void(0)"><i class="jstree-icon jstree-themeicon fa fa-star jstree-themeicon-custom" role="presentation"></i>' + value.username + '</a></li>';
+                        userHtmlTempMini = userHtmlTempMini + '<li class="jstree-node init_agent jstree-closed"><i class="jstree-icon jstree-ocl" data-idtreepro="' + value.id + '" role="'+ value.owner_id +'"></i><a class="jstree-anchor" href="javascript:void(0)"><i class="jstree-icon jstree-themeicon fa fa-star jstree-themeicon-custom" role="presentation"></i>' + value.username + '</a></li>';
                     }
                 })
 
