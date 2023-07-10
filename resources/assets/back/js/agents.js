f34921c64ba40cdbe2f7d352d504842f15468d15
Estarly Olivar <ztrly2022@gmail.com>
2023-07-10 12:00:48 -0400

    validar ENV mailgum

--- resources/assets/back/js/agents.js
+++ resources/assets/back/js/agents.js
@@ -1581,7 +1581,7 @@ class Agents {
                 }
 
             }).done(function (json) {
-
+                console.log(json)
                 $('#username').text(json.data.user.username);
                 $('#agent_timezone').text(json.data.user.timezone);
                 $('.balance').text(json.data.balance);
