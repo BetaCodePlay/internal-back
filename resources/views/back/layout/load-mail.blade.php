e1c683721fc49fbab2c41ef1e63928ec96a6bf24
genesis <genesisperez09052018@gmail.com>
2023-07-12 10:50:17 -0400

    cambios en vista

---
+++ resources/views/back/layout/load-mail.blade.php
@@ -0,0 +1,16 @@
+<div class="col">
+    <div class="noty_bar noty_type__warning noty_theme__unify--v1--dark g-mb-25">
+        <div class="noty_body">
+            <div class="g-mr-20">
+                <div class="noty_body__icon">
+                    <i class="hs-admin-info"></i>
+                </div>
+            </div>
+            <div>
+                <p>
+                    {{ _i('The agent selector is loaded by the session currency, verify that the user has the session currency.') }}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
