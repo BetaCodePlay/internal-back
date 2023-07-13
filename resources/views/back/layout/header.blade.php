ec996aec87f26421aacdf108860630afd01b510b
Estarly Olivar <ztrly2022@gmail.com>
2023-07-13 18:28:35 -0400

    header in array

--- resources/views/back/layout/header.blade.php
+++ resources/views/back/layout/header.blade.php
@@ -32,15 +32,14 @@
                     <form id="header-search-form" class="u-header--search col-sm g-py-12 g-ml-15--sm g-ml-20--md g-mr-10--sm"
                           aria-labelledby="searchInvoker" action="{{ route('users.search') }}" method="get">
                         <div class="input-group g-max-width-450">
-{{--                            @if(in_array(\Dotworkers\Security\Enums\Roles::$super_admin, session()->get('roles')))--}}
-                            @can('access', [\Dotworkers\Security\Enums\Permissions::$users_search])
+{{--                            Permission of search advanced --}}
+                            @can('access', [\Dotworkers\Security\Enums\Permissions::$advanced_users_search])
                                 <input class="form-control form-control-md g-rounded-4" type="text" name="username" placeholder="{{ _i('Search user') }}" value="{{ isset($username) ? $username : '' }}">
                                 <button type="submit"
                                         class="btn u-btn-outline-primary g-brd-none g-bg-transparent--hover g-pos-abs g-top-0 g-right-0 d-flex g-width-40 h-100 align-items-center justify-content-center g-font-size-18 g-z-index-2">
                                     <i class="hs-admin-search"></i>
                                 </button>
                             @endcan
-{{--                            @endif--}}
                         </div>
                     </form>
 {{--                    <a id="searchInvoker" class="g-hidden-sm-up text-uppercase u-header-icon-v1 g-pos-rel g-width-40 g-height-40 rounded-circle g-font-size-20" href="#!" aria-controls="header-search-form" aria-haspopup="true" aria-expanded="false" data-is-mobile-only="true" data-dropdown-event="click"--}}
