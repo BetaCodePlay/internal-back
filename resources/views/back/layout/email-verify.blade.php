2fea92d8fea21aa6a671d43243000a1d8ddda552
Clandor <carlosmejias1000@gmail.com>
2023-07-15 12:05:12 -0400

    email

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -1,6 +1,6 @@
 {{--@if($confirmation_email)--}}
 <div class="alert alert-warning" role="alert">
-    <h4 class="alert-heading">Importante!</h4>
+    <h4 class="alert-heading"><i class="fa fa-envelope-open-o" aria-hidden="true"></i> Correo electronico no verificado</h4>
     <p>Parece que tu correo aun no esta verificado, puedes realizar ahora para mayor seguridad y mejores ofertas.</p>
     <hr>
     <p class="mb-0">
