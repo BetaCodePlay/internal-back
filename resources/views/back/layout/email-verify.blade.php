23a2c60777d56efc51cd8424e4262debf5f3c681
Clandor <carlosmejias1000@gmail.com>
2023-07-15 12:02:21 -0400

    email

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -1,13 +1,12 @@
 {{--@if($confirmation_email)--}}
-    <div class="alert alert-warning" role="alert">
-        <h4 class="alert-heading">Importante!</h4>
-        <p>Parece que tu correo aun no esta verificado, puedes realizar ahora para mayor seguridad y mejores ofertas.</p>
-        <hr>
-        <p class="mb-0">
-            <a href="#">Verificar</a>
-            |
-            <a href="#">Rechazar</a>
-        </p>
-    </div>
-{{--
-@endif--}}
+<div class="alert alert-warning" role="alert">
+    <h4 class="alert-heading">Importante!</h4>
+    <p>Parece que tu correo aun no esta verificado, puedes realizar ahora para mayor seguridad y mejores ofertas.</p>
+    <hr>
+    <p class="mb-0">
+        <a href="javascript:void(0)" data-toggle="modal" data-target="#reset-email-modal">Verificar</a>
+        |
+        <a href="#">Rechazar</a>
+    </p>
+</div>
+
