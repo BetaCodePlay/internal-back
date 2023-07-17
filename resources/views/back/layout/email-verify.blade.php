6f3093fb58cb1f689661030b30b8fcb19140e13a
genesis <genesisperez09052018@gmail.com>
2023-07-17 15:53:40 -0400

    prueba

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -1,7 +1,7 @@
-{{--@if($confirmation_email)--}}
 <div class="alert alert-warning" role="alert">
+@if (isset($mailgun_notifications->$locale->message) && !is_null($mailgun_notifications->$locale->message))
     <h4 class="alert-heading"><i class="fa fa-envelope-o" aria-hidden="true"></i> Correo electrónico no verificado</h4>
-    <p>Parece que tu correo aun no esta verificado, puedes realizar ahora para mayor seguridad y mejores ofertas.</p>
+    <p>{!! $mailgun_notifications->$locale->message !!}</p>
     <hr style="margin: 1rem 0">
     <p class="mb-0">
         <a href="javascript:void(0)" data-toggle="modal" data-target="#reset-email-modal" class="text-primary">Verificar</a>
@@ -9,4 +9,5 @@
         <a href="#">Rechazar</a>
     </p>
 </div>
+@endif
 
