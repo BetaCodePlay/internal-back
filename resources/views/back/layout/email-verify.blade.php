36507c12d31869f46a3fcb597f8ffa44b35ac32f
genesis <genesisperez09052018@gmail.com>
2023-07-17 18:14:28 -0400

    prueba

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -1,13 +1,15 @@
 <div class="alert alert-warning" role="alert">
-@if (isset($mailgun_notifications->$locale->message) && !is_null($mailgun_notifications->$locale->message))
-    <h4 class="alert-heading"><i class="fa fa-envelope-o" aria-hidden="true"></i> Correo electrónico no verificado</h4>
+    @if (isset($mailgun_notifications->$locale->title) && !is_null($mailgun_notifications->$locale->title))
+    <h4 class="alert-heading"><i class="fa fa-envelope-o" aria-hidden="true"></i> {!! $mailgun_notifications->$locale->title !!}</h4>
+    @endif
+    @if (isset($mailgun_notifications->$locale->message) && !is_null($mailgun_notifications->$locale->message))
     <p>{!! $mailgun_notifications->$locale->message !!}</p>
     <hr style="margin: 1rem 0">
+    @endif
     <p class="mb-0">
-        <a href="javascript:void(0)" data-toggle="modal" data-target="#reset-email-modal" class="text-primary">Verificar</a>
+        <a href="javascript:void(0)" data-toggle="modal" data-target="#reset-email-modal"
+            class="text-primary">Verificar</a>
         |
         <a href="#">Rechazar</a>
     </p>
 </div>
-@endif
-
