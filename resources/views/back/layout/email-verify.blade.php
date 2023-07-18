e2a3b5e091aef2ffc528be056fc1f47532a3ab6b
genesis <genesisperez09052018@gmail.com>
2023-07-18 15:52:51 -0400

    cambios

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -17,7 +17,7 @@
         @if (isset($mailgun_notifications->$locale->denied_button) &&
         !is_null($mailgun_notifications->$locale->denied_button))
             |
-            <a href="{{ route('users.confirmed') }}" value="false"
+            <a href="{{ route('users.confirmed', ['denied' => $user->denied]) }}" value="false"
                id="oculta">{!! $mailgun_notifications->$locale->denied_button !!}</a>
         @endif
     </p>
--- routes/web/users.php
+++ routes/web/users.php
@@ -320,7 +320,7 @@ Route::group(['prefix' => 'users', 'middleware' => ['auth']], function () {
     ]);
 
     // Accepted or denied email
-    Route::get('confirmed/{accepted?}/{denied?}', [
+    Route::get('/user/{denied}', [
         'as' => 'users.confirmed',
         'uses' => 'UsersController@confirmedEmail'
     ]);
