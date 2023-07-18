7d3bc78e3a99636ad92ea5ad003fbcfca7318ad6
genesis <genesisperez09052018@gmail.com>
2023-07-17 21:40:09 -0400

    prueba

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -1,15 +1,29 @@
-<div class="alert alert-warning" role="alert">
+<div class="alert alert-warning" id="DivAEsconder" role="alert">
     @if (isset($mailgun_notifications->$locale->title) && !is_null($mailgun_notifications->$locale->title))
-    <h4 class="alert-heading"><i class="fa fa-envelope-o" aria-hidden="true"></i> {!! $mailgun_notifications->$locale->title !!}</h4>
+    <h4 class="alert-heading"><i class="fa fa-envelope-o" aria-hidden="true"></i> {!!
+        $mailgun_notifications->$locale->title !!}</h4>
     @endif
     @if (isset($mailgun_notifications->$locale->message) && !is_null($mailgun_notifications->$locale->message))
     <p>{!! $mailgun_notifications->$locale->message !!}</p>
     <hr style="margin: 1rem 0">
     @endif
     <p class="mb-0">
+        @if (isset($mailgun_notifications->$locale->accepted_button) &&
+        !is_null($mailgun_notifications->$locale->accepted_button))
         <a href="javascript:void(0)" data-toggle="modal" data-target="#reset-email-modal"
-            class="text-primary">Verificar</a>
+            class="text-primary">{!! $mailgun_notifications->$locale->accepted_button !!}</a>
+        @endif
+        @if (isset($mailgun_notifications->$locale->denied_button) &&
+        !is_null($mailgun_notifications->$locale->denied_button))
         |
-        <a href="#">Rechazar</a>
+        <a href="#" id="BotonParaEsconder">{!! $mailgun_notifications->$locale->denied_button !!}</a>
+        @endif
     </p>
 </div>
+<script>
+$(function(){
+        $("#BotonParaEsconder").click(function(){
+            $("#DivAEsconder").hide();
+        });
+    });
+</script> 
