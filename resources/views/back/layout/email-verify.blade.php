8dfeee425d01e283bab4c7ab1e08cc3212902098
genesis <genesisperez09052018@gmail.com>
2023-07-19 10:13:15 -0400

    prueba

--- resources/views/back/layout/email-verify.blade.php
+++ resources/views/back/layout/email-verify.blade.php
@@ -17,7 +17,7 @@
         @if (isset($mailgun_notifications->$locale->denied_button) &&
         !is_null($mailgun_notifications->$locale->denied_button))
             |
-            <a href="" value="false"
+            <a href="{{ route('users.confirmed', ['denied' => false]) }}" value="false"
                id="oculta">{!! $mailgun_notifications->$locale->denied_button !!}</a>
         @endif
     </p>
