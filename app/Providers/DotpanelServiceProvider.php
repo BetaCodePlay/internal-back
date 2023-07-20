91aa8367cd748ca8c64aaf2471fc9eb8be1b979e
genesis <genesisperez09052018@gmail.com>
2023-07-20 12:00:53 -0400

    index controller

--- app/Providers/DotpanelServiceProvider.php
+++ app/Providers/DotpanelServiceProvider.php
@@ -100,6 +100,11 @@ class DotpanelServiceProvider extends ServiceProvider
                     if(($browser== "Safari") && ($agent->isMobile() || $agent->isPhone() || $agent->isTablet())){
                         $iphone = 1;
                     }
+                    $user = auth()->user()->id;
+                    $userData = $this->usersRepo->getUsers($user);
+                    foreach ($userData as $users){
+                        $confirmation = $users->confirmation_email;
+                    }
                     $languagesData = $coreCollection->formatLanguages($languages);
                     $selectedLanguage = $coreCollection->formatSelectedLanguage($language);
                     $timezones = $coreCollection->formatTimezones();
@@ -126,6 +131,7 @@ class DotpanelServiceProvider extends ServiceProvider
                     $data['mailgun_notifications'] = Configurations::getMailgunNotifications();
                     $data['reset_main_password'] = Configurations::getResetMainPassword();
                     $data['locale'] = LaravelGettext::getLocale();
+                    $data['confirmation_email'] = $confirmation;
                     //dd($data);
                     view()->share($data);
                 } catch (\Exception $ex) {
