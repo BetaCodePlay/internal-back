dedb86e361ca6de5f2e17220473d724c1f081044
Mayinis <hachhe.dev@gmail.com>
2023-07-26 16:45:14 -0400

    Cambio de nombre en form paypal

--- app/Http/Controllers/BetPayController.php
+++ app/Http/Controllers/BetPayController.php
@@ -2045,7 +2045,7 @@ class BetPayController extends Controller
                         ];
                         return Utils::successResponse($data);
                     } else {
-                        return Utils::errorResponse(Codes::$forbidden, $response->data->message);
+                        return Utils::errorResponse(Codes::$forbidden, $response->data);
                     }
 
                 }else{
