12b74a2585399d39cad67926c180b650e2973e13
genesis <genesisperez09052018@gmail.com>
2023-08-15 12:01:10 -0400

    featured section

--- app/Core/Repositories/SectionImagesRepo.php
+++ app/Core/Repositories/SectionImagesRepo.php
@@ -35,6 +35,7 @@ class SectionImagesRepo
      */
     public function allByElementType($templateElementType)
     {
+        \Log::notice(__METHOD__, ['$templateElementType' => $templateElementType]);
         return SectionImage::where('element_type_id', $templateElementType)
             ->whitelabel()
             ->get();
