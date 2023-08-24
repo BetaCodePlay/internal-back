f66b21ca7fb9427e76e8dca30d9e980a6bde17f0
Mayinis <hachhe.dev@gmail.com>
2023-08-24 11:49:00 -0400

    cambio vista paypal

--- resources/views/back/betpay/clients/payment-methods/paypal.blade.php
+++ resources/views/back/betpay/clients/payment-methods/paypal.blade.php
@@ -11,9 +11,20 @@
         </div>
     </div>
     <div class="col-md-6 d-none paypal">
-        <div class="text-left">
-            <p>
-                {{ _i('Attention! Please note that fees will apply to paypal transactions.') }}
-            </p>
-        </div> 
+        <div class="noty_bar noty_type__warning noty_theme__unify--v1--dark g-mb-25">
+            <div class="noty_body">
+                    <div class="g-mr-20">
+                        <div class="noty_body__icon">
+                            <i class="hs-admin-info"></i>
+                        </div>
+                    </div>
+                    <div>
+                        <p>
+                            {{ _i('Attention! Please note that fees will apply to paypal transactions') }}
+                        </p>
+                    </div>
+            </div>
+        </div>
     </div>
+    
+                            
