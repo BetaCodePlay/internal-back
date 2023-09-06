359dc6f4e6afcf998aa5d705dca16ce58d18317d
Estarly Olivar <ztrly2022@gmail.com>
2023-09-05 21:39:15 -0400

    rol agent and all wl

--- app/Core/helpers.php
+++ app/Core/helpers.php
@@ -239,27 +239,27 @@ if (!function_exists('menu')) {
                         'submenu' => []
                     ],
 
-                    //Create Agent
-                    'AgentsCreateAgent' => [
-                        'text' => _i('Create agent user'),
-                        'level_class' => 'second',
-                        'route' => 'agents.create.agent',
-                        'params' => [],
-                        'icon' => 'hs-admin-user',
-                        'permission' => Permissions::$create_user_agent,
-                        'submenu' => []
-                    ],
-
-                    //Create Player
-                    'AgentsCreateUser' => [
-                        'text' => _i('Create player user'),
-                        'level_class' => 'second',
-                        'route' => 'agents.create.user',
-                        'params' => [],
-                        'icon' => 'hs-admin-user',
-                        'permission' => Permissions::$agents_dashboard,
-                        'submenu' => []
-                    ],
+//                    //Create Agent
+//                    'AgentsCreateAgent' => [
+//                        'text' => _i('Create agent user'),
+//                        'level_class' => 'second',
+//                        'route' => 'agents.create.agent',
+//                        'params' => [],
+//                        'icon' => 'hs-admin-user',
+//                        'permission' => Permissions::$create_user_agent,
+//                        'submenu' => []
+//                    ],
+//
+//                    //Create Player
+//                    'AgentsCreateUser' => [
+//                        'text' => _i('Create player user'),
+//                        'level_class' => 'second',
+//                        'route' => 'agents.create.user',
+//                        'params' => [],
+//                        'icon' => 'hs-admin-user',
+//                        'permission' => Permissions::$agents_dashboard,
+//                        'submenu' => []
+//                    ],
 
                     'AddAgentUsers' => [
                         'text' => _i('Add users'),
