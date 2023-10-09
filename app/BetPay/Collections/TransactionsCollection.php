12373ca61aa1be733ef3bbfe1c19d8ac8bc35c59
Mayinis <hachhe.dev@gmail.com>
2023-10-09 14:01:29 -0400

    report mercadopago

--- app/BetPay/Collections/TransactionsCollection.php
+++ app/BetPay/Collections/TransactionsCollection.php
@@ -628,7 +628,6 @@ class TransactionsCollection
                 }
                 case PaymentMethods::$paypal:
                 {
-                    \Log::debug([$transaction]);
                     $transaction->withdrawal_data .= sprintf(
                         '<strong>%s:</strong> %s<br>',
                         _i('Email'),
