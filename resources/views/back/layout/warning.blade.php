bb3a7a95298711ca600ba60dc828ddfcff59afc2
Clandor <carlosmejias1000@gmail.com>
2023-10-16 14:05:15 -0400

    warning

--- resources/views/back/layout/warning.blade.php
+++ resources/views/back/layout/warning.blade.php
@@ -2,14 +2,14 @@
     <div class="container">
         <div class="warning-container">
             <div class="warning-title">ATENCION NOTICIAS</div>
-            {{--<div class="warning-general-template-ex">
-                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
-                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
-            </div>--}}
             <div class="warning-general-template-ex">
-                <div class="msg">Le informamos que el día de mañana a las 8 am hora Argentina la plataforma estará en mantenimiento debido a unas actualizaciones que se harán en el sistema con el fin de mejorar. Le informaremos cuando haya culminado la actualización y la plataforma esté activa de nuevo.</div>
-                <div class="msg">Le informamos que el día de mañana a las 8 am hora Argentina la plataforma estará en mantenimiento debido a unas actualizaciones que se harán en el sistema con el fin de mejorar. Le informaremos cuando haya culminado la actualización y la plataforma esté activa de nuevo.</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
+                <div class="msg">La página prohíbe que sus agentes establezcan reglas como apuestas mínimas o maximas con el fin de limitar a los usuarios, evite sanciones.</div>
             </div>
+            {{--<div class="warning-general-template-ex">
+                <div class="msg">Le informamos que el día de mañana a las 8 am hora Argentina la plataforma estará en mantenimiento debido a unas actualizaciones que se harán en el sistema con el fin de mejorar. Le informaremos cuando haya culminado la actualización y la plataforma esté activa de nuevo.</div>
+                <div class="msg">Le informamos que el día de mañana a las 8 am hora Argentina la plataforma estará en mantenimiento debido a unas actualizaciones que se harán en el sistema con el fin de mejorar. Le informaremos cuando haya culminado la actualización y la plataforma esté activa de nuevo.</div>
+            </div>--}}
         </div>
     </div>
 </div>
