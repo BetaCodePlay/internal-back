fb997aceca4c694c67580e76f94266f4b404feea
Clandor <carlosmejias1000@gmail.com>
2023-10-18 13:07:53 -0400

    back

--- resources/views/back/agents/modals/info-bonus.blade.php
+++ resources/views/back/agents/modals/info-bonus.blade.php
@@ -2,46 +2,36 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">{{ _i('Bonus requirements') }}</h5>
+                <h5 class="modal-title">Términos y condiciones del Bono</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <ul>
+                <ul style="padding-left: 20px">
                     <li><b>1</b>. Cada vez que deposites se te asignará el 200% de tu carga en saldo de billetera de
                         bono.
                         Por ejemplo: si cargas 1.000,00$ se le dará un bono de 2.000,00$ ya que porcentaje configurado
                         es de 200%
                     </li>
-                </ul>
-                <ul>
                     <li>
                         <b>2</b>. Para retirar saldo de tu billetera de bonos tienes que tener el doble de tu monto
                         inicial obtenido. Luego que obtengas el doble del bono otorgado lo excedente pasará a tu
                         billetera de saldo real. Por ejemplo, si obtienes 1.000 ARS de bono, deberás tener 2.000 ARS
                         para que lo siguiente en ganar sea acreditado en billetera de saldo real.
                     </li>
-                </ul>
-                <ul>
                     <li>
                         <b>3</b>. En caso de solicitar el retiro de todo tu dinero de saldo de billetera real, tu bono
                         será cancelado quedando en cero ambas billeteras.
                     </li>
-                </ul>
-                <ul>
                     <li>
                         <b>4</b>. Las apuestas que realices serán debitadas 90% de saldo real y el 10% de tu saldo de
                         billetera de bono. Es decir, si haces una apuesta de 100 ARS, se descuentan 90 ARS de saldo de
                         billetera real y 10 ARS de saldo de billetera de bonos.
                     </li>
-                </ul>
-                <ul>
                     <li>
                         <b>5</b>. Podrás obtener este bono cada vez que realices una carga en tu billetera.
                     </li>
-                </ul>
-                <ul>
                     <li>
                         <b>6</b>. El monto máximo a obtener como bono es de 50.000 ARS aún cuando cargues 100.000 ARS.
                         Es decir que a pesar de que cargues montos mayores a 25.000 ARS solo se te acreditará como monto
