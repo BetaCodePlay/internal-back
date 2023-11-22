ba59583afcc591417c8ab5f505ab25cca63030b6
Clandor <carlosmejias1000@gmail.com>
2023-11-22 18:06:29 -0400

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,15 +38,17 @@
                         {{ _i('Sign in to start managing your project.')}}
                     </div>
                     <div class="login-form-line">
+                        <label>E-mail</label>
                         <div class="wrap-input-login validate-input">
-                            <input class="input-login" type="text" name="username" id="username" autocomplete="off" placeholder="{{ _i('Username')}}" required>
+                            <input class="input-login" type="text" name="username" id="username" autocomplete="off" placeholder="{{ _i('example@email.com')}}" required>
                         </div>
 
+                        <label>Password</label>
                         <div class="wrap-input-login validate-input" data-validate="{{ _i('Enter password')}}">
 						<span class="btn-show-pass">
 							<i class="fa fa-eye"></i>
 						</span>
-                            <input class="input-login" type="password" name="password" id="password" autocomplete="off" placeholder="{{ _i('Password') }}" required>
+                            <input class="input-login" type="password" name="password" id="password" autocomplete="off" placeholder="{{ _i('At least 8 characters') }}" required>
                         </div>
 
                         <div class="wrap-input-login">
