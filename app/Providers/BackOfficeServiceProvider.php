fb77ccdb2759a9c2c60f5e430889e08a0dd2dc94
Emilio Hernandez <emilio25informatic@gmail.com>
2023-11-22 14:34:42 -0300

    boot anterior

--- app/Providers/BackOfficeServiceProvider.php
+++ app/Providers/BackOfficeServiceProvider.php
@@ -7,10 +7,12 @@ use App\Core\Collections\CurrenciesCollection;
 use App\Core\Collections\PushNotificationsCollection;
 use App\Core\Repositories\CurrenciesRepo;
 use App\Core\Repositories\PushNotificationsRepo;
+use App\Users\Repositories\UsersRepo;
 use Dotworkers\Configurations\Configurations;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
@@ -23,6 +25,123 @@ use Xinax\LaravelGettext\Facades\LaravelGettext;
  */
 class BackOfficeServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap any application services
+     *
+     * @param Request $request
+     * @param CoreCollection $coreCollection
+     * @param PushNotificationsRepo $pushNotificationsRepo
+     * @param PushNotificationsCollection $pushNotificationsCollection
+     * @param UsersRepo $usersRepo
+     * @param CurrenciesRepo $currenciesRepo
+     * @param Agent $agent
+     */
+    public function boot(Request $request, CoreCollection $coreCollection, UsersRepo $usersRepo, PushNotificationsRepo $pushNotificationsRepo, PushNotificationsCollection $pushNotificationsCollection, CurrenciesRepo $currenciesRepo, CurrenciesCollection $currenciesCollection, Agent $agent)
+    {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $regex = '/^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$/';
+            $domain = strtolower($_SERVER['HTTP_HOST']);
+            $iphone = 0;
+
+            if ($request->path() == 'elb-health-check') {
+                $domain = 'dotworkers.net';
+            }
+
+            if (preg_match($regex, $domain)) {
+                die;
+            }
+
+            if($domain == 'back-office-v1.co' || $domain == 'back-office.co'){
+                $domain = 'back-office.co';
+            }
+
+            $configurations = Configurations::getConfigurationsByURL($domain);
+
+            if (count($configurations) == 0) {
+                $domain = str_replace('dotpanel.', '', $domain);
+                $configurations = Configurations::getConfigurationsByDomain($domain);
+            }
+
+            if (count($configurations) > 0) {
+                try {
+                    config(['whitelabels.configurations' => $configurations]);
+                    config(['whitelabels.whitelabel_status' => $configurations[0]->whitelabel_status]);
+                    $language = $request->cookie('language-js');
+                    $languages = Configurations::getLanguages();
+
+                    if (is_null($language)) {
+                        foreach ($languages as $item) {
+                            $shortItem = substr($item, 0, 2);
+                            if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+                                $browserLanguage = str_replace('-', '_', substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2));
+
+                                if ($browserLanguage == $shortItem) {
+                                    $language = $item;
+                                }
+                            }
+                        }
+
+                        $language = is_null($language) ? Configurations::getDefaultLanguage() : $language;
+                        cookie('language', $language, $minutes = 525600);
+                        App::setLocale(substr($language, 0, 2));
+                    }
+                    LaravelGettext::setLocale($language);
+
+                    if ($this->app->environment() != 'local') {
+                        Configurations::setEmail();
+                    }
+
+                    $whitelabel = Configurations::getWhitelabel();
+                    $whitelabels = [45];
+                    if (!in_array($whitelabel, $whitelabels)) {
+                        if ((env('APP_ENV') == 'production') || (env('APP_ENV') == 'testing') ) {
+                            URL::forceScheme('https');
+                        }
+                    }
+                    $browser = $agent->browser();
+                    if(($browser== "Safari") && ($agent->isMobile() || $agent->isPhone() || $agent->isTablet())){
+                        $iphone = 1;
+                    }
+
+                    $languagesData = $coreCollection->formatLanguages($languages);
+                    $selectedLanguage = $coreCollection->formatSelectedLanguage($language);
+                    $timezones = $coreCollection->formatTimezones();
+                    $pushNotifications = $pushNotificationsRepo->getUnread(Configurations::getWhitelabel());
+                    $allCurrencies = $currenciesRepo->all();
+                    $currenciesCollection->formatAll($allCurrencies);
+                    $whitelabelCurrencies = Configurations::getCurrencies();
+                    $whitelabelCurrencies = $currenciesCollection->formatWhitelabelCurrencies($whitelabelCurrencies, $allCurrencies);
+                    $data['push_notifications'] = $pushNotificationsCollection->formatAll($pushNotifications);
+                    $data['push_notifications_quantity'] = count($pushNotifications);
+                    $data['favicon'] = Configurations::getFavicon();
+                    $data['whitelabel_description'] = Configurations::getWhitelabelDescription();
+                    $data['whitelabel_info'] = Configurations::getWhitelabelInfo();
+                    $data['languages'] = $languagesData;
+                    $data['selected_language'] = $selectedLanguage;
+                    $data['currencies'] = Configurations::getCurrencies();
+                    $data['whitelabel_currencies'] = $whitelabelCurrencies;
+                    $data['all_currencies'] = $allCurrencies;
+                    $data['global_timezones'] = $timezones;
+                    $data['free_currency'] = Configurations::getFreeCurrency();
+                    $data['logo'] = Configurations::getLogo($mobile = true);
+                    $data['iphone'] = $iphone;
+                    $data['theme'] = Configurations::getTheme();
+                    $data['mailgun_notifications'] = Configurations::getMailgunNotifications();
+                    $data['reset_main_password'] = Configurations::getResetMainPassword();
+                    $data['locale'] = LaravelGettext::getLocale();
+                    //dd($data);
+                    view()->share($data);
+                } catch (\Exception $ex) {
+                    Log::error(__METHOD__, ['exception' => $ex, 'request' => $request->all(), 'domain' => $domain]);
+                    die;
+                }
+            } else {
+                die;
+            }
+        }
+    }
+
+    /**
     /**
      * Configure white-label information.
      *
@@ -92,7 +211,7 @@ class BackOfficeServiceProvider extends ServiceProvider
      *
      * @throws InvalidArgumentException
      */
-    public function boot(
+    public function bootNew(
         Request $request,
         CoreCollection $coreCollection,
         PushNotificationsRepo $pushNotificationsRepo,
