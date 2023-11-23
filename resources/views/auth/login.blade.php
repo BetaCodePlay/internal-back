bf93916eb49683a526916d83851b4a4f6db9f625
Clandor <carlosmejias1000@gmail.com>
2023-11-23 13:02:49 -0400

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -129,7 +129,7 @@
 <script src="{{ mix('js/manifest.js', 'auth') }}"></script>
 <script src="{{ mix('js/vendor.js', 'auth') }}"></script>
 <script src="{{ mix('js/custom.min.js', 'auth') }}"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
     class Toastr {
         static notifyToastr(title, message, type, timeout) {
