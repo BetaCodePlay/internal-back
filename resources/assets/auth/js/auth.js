f9519024dac6d1373d480e3d53989f558ca03e76
Clandor <carlosmejias1000@gmail.com>
2023-11-25 13:55:01 -0400

    reset paswword

--- resources/assets/auth/js/auth.js
+++ resources/assets/auth/js/auth.js
@@ -38,6 +38,10 @@ class Auth {
         function changePassword() {
             $('.modal-reset-password').fadeIn();
 
+            setTimeout(function (){
+                $('#reset-password').focus();
+            }, 300);
+
             $(document).on('click', '.btn-reset-password', function () {
                 let $this = $(this);
                 let $modal = $('.modal-reset-password');
