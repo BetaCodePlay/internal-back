6269ee6d137e45450acecc5f59f3324e51f226c0
Clandor <carlosmejias1000@gmail.com>
2023-11-28 16:17:55 -0400

    auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>{{ $title }}</title>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="csrf-token" content="">
-    <!--===============================================================================================-->
-    <link rel="shortcut icon" href="{{ $favicon }}">
-    <link rel="apple-touch-icon" sizes="57x57" href="{{ $favicon }}">
-    <link rel="apple-touch-icon" sizes="114x114" href="{{ $favicon }}">
-    <!--===============================================================================================-->
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
-    <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
-    <!--===============================================================================================-->
-</head>
-<body class="body-login">
+@extends('auth.template')
 
-<div class="limiter">
+@section('content')
     <div class="container-login">
         <div class="wrap-login">
             <div class="login-preview-bg" style="background-image: url('https://bestcasinos-llc.s3.us-east-2.amazonaws.com/templates/bg-login-v2.jpg')">
@@ -87,56 +68,21 @@
             </form>
         </div>
     </div>
-</div>
+@endsection
 
-{{--<footer>
+@section('modals')
+    @include('auth.modals.change-password')
+@endsection
 
-    <div class="dropdown text-center p-b-20">
-        <div class="languages-menu" id="languages-menu">
-            <a class="languages-menu-selected">
-                <img class="lang-flag" src="{{ $selected_language['flag'] }}"
-                     alt="{{ $selected_language['name'] }}">
-                <span class="title-lang">
-            {{ $selected_language['name'] }}
-            <i class="fa fa-caret-down" aria-hidden="true"></i>
-        </span>
-            </a>
-            <ul class="languages-submenu">
-                @foreach ($languages as $language)
-                    <li>
-                        <a href="{{ route('core.change-language', [$language['iso']]) }}"
-                           class="change-language" data-locale="{{ $language['iso'] }}">
-                            <img class="lang-flag" src="{{ $language['flag'] }}" alt="{{ $language['name'] }}">
-                            <span>{{ $language['name'] }}</span>
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
-        </div>
-        <div id="dropDownSelect1"></div>
+@section('scripts')
+    <script>
+        $(function () {
+            Auth.login();
+        });
 
-    </div>
-    <p class="text-center copyright-text">
-        {{ $whitelabel_info->copyright ? _i('Developed by Betsweet. Operated by') : '' }} {{ $whitelabel_description }}
-        © {{ _i('Copyright') }} - {{ date('Y') }}. {{ _i('All rights reserved') }}
-    </p>
-
-
-</footer>--}}
-@include('auth.modals.change-password')
-
-<script src="{{ mix('js/manifest.js', 'auth') }}"></script>
-<script src="{{ mix('js/vendor.js', 'auth') }}"></script>
-<script src="{{ mix('js/custom.min.js', 'auth') }}"></script>
-<script>
-    $(function () {
-        Auth.login();
-    });
-
-    $(window).on('load', function() {
-        $('.loader-login').hide();
-        $('.login-form-ex').addClass('load');
-    });
-</script>
-</body>
-</html>
+        $(window).on('load', function () {
+            $('.loader-login').hide();
+            $('.login-form-ex').addClass('load');
+        });
+    </script>
+@endsection
---
+++ resources/views/auth/template.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{{ $title }}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="">
+    <!--===============================================================================================-->
+    <link rel="shortcut icon" href="{{ $favicon }}">
+    <link rel="apple-touch-icon" sizes="57x57" href="{{ $favicon }}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{ $favicon }}">
+    <!--===============================================================================================-->
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
+    <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
+    <!--===============================================================================================-->
+</head>
+<body class="body-auth">
+<div class="limiter">
+    @yield('content')
+</div>
+
+
+@yield('modals')
+
+<script src="{{ mix('js/manifest.js', 'auth') }}"></script>
+<script src="{{ mix('js/vendor.js', 'auth') }}"></script>
+<script src="{{ mix('js/custom.min.js', 'auth') }}"></script>
+@yield('scripts')
+</body>
+</html>
