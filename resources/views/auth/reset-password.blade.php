36ebfeb7a776c2c319e3d4ec85f10b7e935af2a7
Clandor <carlosmejias1000@gmail.com>
2023-11-28 22:47:44 -0400

    auth

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -5,19 +5,21 @@
         <div class="container-auth-bg" style="background-image: url('https://bestcasinos-llc.s3.us-east-2.amazonaws.com/templates/bg-login-v2.jpg')"></div>
         <div class="bg-opacity"></div>
         <div class="content">
-            <div class="auth-title">{{ _i('We send you an email.') }}</div>
-            <div class="auth-subtitle">{{ _i('To reset the account we send you a link to your email for security, enter the new password below.') }}</div>
-            <div class="auth-body">
-                <label>{{ _i('Password')}}</label>
-                <div class="wrap-input-login validate-input" data-validate="{{ _i('Enter password')}}">
+            <div class="content-ex">
+                <div class="auth-title">{{ _i('We send you an email.') }}</div>
+                <div class="auth-subtitle">{{ _i('To reset the account we send you a link to your email for security, enter the new password below.') }}</div>
+                <div class="auth-body">
+                    <label>{{ _i('Password')}}</label>
+                    <div class="wrap-input-login validate-input" data-validate="{{ _i('Enter password')}}">
 						<span class="btn-show-pass">
 							<i class="fa fa-eye-slash"></i>
 						</span>
-                    <input class="input-login" type="password" name="password" id="reset-password" autocomplete="off" placeholder="{{ _i('At least 8 characters') }}" required>
+                        <input class="input-login" type="password" name="password" id="reset-password" autocomplete="off" placeholder="{{ _i('At least 8 characters') }}" required>
+                    </div>
+                    <button type="button" class="btn-reset-password" data-route="{{ route('auth.change-password') }}" data-loading-text="<i class='fa fa-spin fa-spinner'></i> {{ _i('Please wait...') }}">
+                        {{ _i('Reset') }}
+                    </button>
                 </div>
-                <button type="button" class="btn-reset-password" data-route="{{ route('auth.change-password') }}" data-loading-text="<i class='fa fa-spin fa-spinner'></i> {{ _i('Please wait...') }}">
-                    {{ _i('Reset') }}
-                </button>
             </div>
         </div>
     </div>
