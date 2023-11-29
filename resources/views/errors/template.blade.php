e65e58c27d1f23436a729c2f6b0cf3de7b3fd7b4
Clandor <carlosmejias1000@gmail.com>
2023-11-29 15:51:45 -0400

    auth

--- resources/views/errors/template.blade.php
+++ resources/views/errors/template.blade.php
@@ -1,15 +1,39 @@
-@extends('auth.template')
-
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('code')</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="">
+    <!--===============================================================================================-->
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
+    <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
+    <!--===============================================================================================-->
+</head>
+<body class="body-auth">
+<div class="limiter">
     <div class="container-auth">
         <div class="container-auth-bg" style="background-image: url('https://bestcasinos-llc.s3.us-east-2.amazonaws.com/templates/bg-login-v2.jpg')"></div>
         <div class="bg-opacity"></div>
         <div class="content">
             <div class="content-ex">
-                <div class="auth-figure"><img class="login-logo" src="{{  $logo->img_dark }}" alt="{{ $whitelabel_description }}"></div>
+                <div class="auth-figure"><img src="https://bestcasinos-llc.s3.us-east-2.amazonaws.com/templates/air-decorate-auth.png"></div>
                 <div class="auth-title">@yield('code')</div>
                 <div class="auth-subtitle">@yield('message')</div>
             </div>
         </div>
     </div>
-@endsection
+</div>
+
+
+@yield('modals')
+
+<script src="{{ mix('js/manifest.js', 'auth') }}"></script>
+<script src="{{ mix('js/vendor.js', 'auth') }}"></script>
+<script src="{{ mix('js/custom.min.js', 'auth') }}"></script>
+@yield('scripts')
+</body>
+</html>
