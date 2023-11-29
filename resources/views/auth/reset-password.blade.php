ed815985eac8fc3b7436ef3c2fec5ac5b9c93bda
Clandor <carlosmejias1000@gmail.com>
2023-11-28 22:29:59 -0400

    auth

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,5 +1,13 @@
 @extends('auth.template')
 
 @section('content')
-<h1>Reset password</h1>
+    <div class="container-auth">
+        <div class="container-auth-bg" style="background-image: url('https://bestcasinos-llc.s3.us-east-2.amazonaws.com/templates/bg-login-v2.jpg')">
+            <div class="bg-opacity"></div>
+            <div class="content">
+                <h1>Reset password</h1>
+            </div>
+        </div>
+
+    </div>
 @endsection
