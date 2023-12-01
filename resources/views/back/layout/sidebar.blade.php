18c797ca3e9eb8d75159b5999636206ad70fc5e3
Clandor <carlosmejias1000@gmail.com>
2023-11-30 22:12:39 -0400

    password reset

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -2,13 +2,13 @@
     <ul id="sideNavMenu" class="u-sidebar-navigation-v1-menu u-side-nav--top-level-menu g-min-height-100vh mb-0">
         {{--{!! \Core::buildMenu() !!}--}}
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden active" href="#" target="_self">
+            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-solid fa-house-chimney"></i></span> <span class="media-body align-self-center">{{ _i('Home') }}</span>
             </a>
         </li>
 
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
+            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden active" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-solid fa-laptop-code"></i></span> <span class="media-body align-self-center">{{ _i('Dashboard') }}</span>
             </a>
         </li>
