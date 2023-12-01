4a4d98ec8c0e74041bea402b073b41dafbc0fc0a
Clandor <carlosmejias1000@gmail.com>
2023-12-01 16:40:16 -0400

    sidebar

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -29,7 +29,7 @@
 <body class="currency-theme-{{ session('currency') }}">
 @include('back.layout.header')
 <main class="container-fluid px-0 g-pt-65">
-    <div class="row no-gutters g-pos-rel g-overflow-x-hidden">
+    <div class="row no-gutters g-pos-rel">
         @include('back.layout.sidebar')
         <div class="col g-ml-45 g-ml-0--lg g-pb-65--md">
             <div class="g-pa-20">
