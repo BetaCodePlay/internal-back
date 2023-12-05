c79af9b9d81c00cf09b43784370eefea4881c521
Clandor <carlosmejias1000@gmail.com>
2023-12-05 17:45:06 -0400

    sidebar

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -13,18 +13,21 @@
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-solid fa-house-chimney"></i></span> <span class="media-body align-self-center">{{ _i('Home') }}</span>
+                <span class="icon-mobile"><i class="fa-solid fa-chevron-right"></i></span>
             </a>
         </li>
 
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden active" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-solid fa-laptop-code"></i></span> <span class="media-body align-self-center">{{ _i('Dashboard') }}</span>
+                <span class="icon-mobile"><i class="fa-solid fa-chevron-right"></i></span>
             </a>
         </li>
 
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-regular fa-user"></i></span> <span class="media-body align-self-center">{{ _i('Usuarios') }}</span>
+                <span class="icon-mobile"><i class="fa-solid fa-chevron-right"></i></span>
             </a>
         </li>
 
@@ -39,12 +42,14 @@
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-regular fa-bell"></i></span> <span class="media-body align-self-center">{{ _i('Notifications') }}</span>
+                <span class="icon-mobile"><i class="fa-solid fa-chevron-right"></i></span>
             </a>
         </li>
 
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="#" target="_self">
                 <span class="g-pos-rel"><i class="fa-solid fa-gear"></i></span> <span class="media-body align-self-center">{{ _i('Setting') }}</span>
+                <span class="icon-mobile"><i class="fa-solid fa-chevron-right"></i></span>
             </a>
         </li>
 
@@ -63,12 +68,6 @@
                 <span class="g-pos-rel"><i class="fa-solid fa-arrows-left-right-to-line"></i></span> <span class="media-body align-self-center">{{ _i('Collapse') }}</span>
             </a>
         </li>
-
-        {{--<li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12 active" href="https://dev-back.bestcasinos.lat/agents/create-agent" target="_self">
-                <span class="d-flex align-self-center g-pos-rel g-font-size-18 g-mr-18"><i class="hs-admin-bar-chart"></i></span><span class="media-body align-self-center">Crear usuario de agente</span>
-            </a>
-        </li>--}}
     </ul>
 </div>
 
