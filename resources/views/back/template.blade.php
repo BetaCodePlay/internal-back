b19eb7091046ee61c0cff4fb3c9979aeb84bf0b1
Clandor <carlosmejias1000@gmail.com>
2023-12-05 14:39:05 -0400

    sidebar

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -31,7 +31,7 @@
 <main class="container-fluid px-0 g-pt-65">
     <div class="row no-gutters g-pos-rel">
         @include('back.layout.sidebar')
-        <div class="col g-ml-45 g-ml-0--lg g-pb-65--md">
+        <div class="col">
             <div class="g-pa-20">
                 @yield('content')
                 @if(!empty($action))
