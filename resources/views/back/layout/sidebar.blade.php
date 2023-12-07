0a7718f15335286c1e7a89116c36e95c50bccfa3
Clandor <carlosmejias1000@gmail.com>
2023-12-06 21:43:56 -0400

    sidebar

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -32,17 +32,17 @@
             </a>
             <ul id="collapseExample" class="u-sidebar-navigation-v1-menu u-side-nav--second-level-menu mb-0 collapse">
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
-                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12" href="https://staging-back.bestcasinos.lat/agents" target="_self">
+                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden" href="https://staging-back.bestcasinos.lat/agents" target="_self">
                         <span class="media-body align-self-center">Dashboard</span>
                     </a>
                 </li>
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
-                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12" href="https://staging-back.bestcasinos.lat/agents" target="_self">
+                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden" href="https://staging-back.bestcasinos.lat/agents" target="_self">
                         <span class="media-body align-self-center">Agregar usuarios</span>
                     </a>
                 </li>
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
-                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden g-px-15 g-py-12" href="https://staging-back.bestcasinos.lat/agents" target="_self">
+                    <a class="d-flex media u-side-nav--second-level-menu-link u-side-nav--hide-on-hidden" href="https://staging-back.bestcasinos.lat/agents" target="_self">
                         <span class="media-body align-self-center">Reportes</span>
                     </a>
                 </li>
