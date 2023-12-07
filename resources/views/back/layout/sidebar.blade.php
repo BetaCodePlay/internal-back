e01fc26672aa046db60f3d0fed68336698d365cb
Clandor <carlosmejias1000@gmail.com>
2023-12-06 23:37:23 -0400

    sidebar

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -34,31 +34,37 @@
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
                     <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
                         <span class="media-body align-self-center">Dashboard</span>
+                        <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                     </a>
                 </li>
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                     <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
                         <span class="media-body align-self-center">Agregar usuarios</span>
+                        <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                     </a>
                 </li>
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                     <a class="media u-side-nav--second-level-menu-link" href="javascript:void(0)" target="_self" data-toggle="collapse" data-target="#collapseExampleTwo" aria-expanded="false">
                         <span class="media-body align-self-center">Reportes</span>
+                        <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                     </a>
                     <ul id="collapseExampleTwo" class="u-sidebar-navigation-v1-menu u-side-nav--second-level-menu mb-0 collapse">
                         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                             <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
                                 <span class="media-body align-self-center">Dashboard</span>
+                                <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                             </a>
                         </li>
                         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
                             <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
                                 <span class="media-body align-self-center">Agregar usuarios</span>
+                                <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                             </a>
                         </li>
                         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                             <a class="media u-side-nav--second-level-menu-link" href="#" target="_self" data-toggle="collapse" data-target="#collapseExampleTwo">
                                 <span class="media-body align-self-center">Reportes</span>
+                                <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                             </a>
                         </li>
                     </ul>
