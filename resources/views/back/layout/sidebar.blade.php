59b1f25ebc506798f62c4e5da23382b2deb40c8e
Orlando Bravo <jbravocadevilla@gmail.com>
2023-12-08 12:37:10 -0400

    sidebar

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -33,7 +33,7 @@
             </a>
             <ul id="collapseExample" class="u-sidebar-navigation-v1-menu u-side-nav--second-level-menu u-side-nav--second-level-menu-top mb-0 collapse show">
                 @can('access', [\Dotworkers\Security\Enums\Permissions::$create_users])
-                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                         <a class="media u-side-nav--second-level-menu-link" href="{{ route('users.create') }}" target="_self">
                             <span class="media-body align-self-center">{{ _i('Create') }}</span>
                             <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
