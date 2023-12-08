6f345fed6330f507ab21b19bbc55006cb8bee0b5
Orlando Bravo <jbravocadevilla@gmail.com>
2023-12-08 12:32:36 -0400

    sidebar

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -25,24 +25,77 @@
                 </a>
             </li>
         @endif
-        <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
+        @can('access', [\Dotworkers\Security\Enums\Permissions::$users_menu])
+            <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="javascript:void(0)" data-toggle="collapse" data-target="#collapseExample" aria-expanded="true">
-                <span class="g-pos-rel"><i class="fa-regular fa-user"></i></span> <span class="media-body align-self-center">{{ _i('Usuarios') }}</span>
+                <span class="g-pos-rel"><i class="fa-regular fa-user"></i></span> <span class="media-body align-self-center">{{ _i('Users') }}</span>
                 <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
             </a>
             <ul id="collapseExample" class="u-sidebar-navigation-v1-menu u-side-nav--second-level-menu u-side-nav--second-level-menu-top mb-0 collapse show">
-                <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
-                    <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
-                        <span class="media-body align-self-center">Dashboard</span>
-                        <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
-                    </a>
-                </li>
-                <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
-                    <a class="media u-side-nav--second-level-menu-link" href="#" target="_self">
-                        <span class="media-body align-self-center">Agregar usuarios</span>
-                        <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
-                    </a>
-                </li>
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$create_users])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('users.create') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Create') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$advanced_users_search])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('users.advanced-search') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Advanced search') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$web_registers])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('reports.users.registered-users') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Registered users') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$users_status])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('users.users-status') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Users status') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$users_balances])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('reports.users.balances') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Users balances') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$users_conversion])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('reports.users.users-conversion') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Users conversion') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$users_logins])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('reports.users.total-logins') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Logins') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
+                @can('access', [\Dotworkers\Security\Enums\Permissions::$temp_users])
+                    <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
+                        <a class="media u-side-nav--second-level-menu-link" href="{{ route('users.temp') }}" target="_self">
+                            <span class="media-body align-self-center">{{ _i('Temp users') }}</span>
+                            <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
+                        </a>
+                    </li>
+                @endif
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item">
                     <a class="media u-side-nav--second-level-menu-link" href="javascript:void(0)" target="_self" data-toggle="collapse" data-target="#collapseExampleTwo" aria-expanded="false">
                         <span class="media-body align-self-center">Reportes</span>
@@ -71,7 +124,7 @@
                 </li>
             </ul>
         </li>
-
+        @endif
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <hr>
         </li>
