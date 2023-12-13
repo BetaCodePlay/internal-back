81d7cb7c02b52a4023f21d2594ab1f2e2da0a606
Clandor <carlosmejias1000@gmail.com>
2023-12-13 00:36:25 -0400

    header

--- resources/views/back/layout/header.blade.php
+++ resources/views/back/layout/header.blade.php
@@ -29,65 +29,34 @@
 
                 <div class="col-auto d-flex g-pt-5 g-pt-0--sm g-pl-5 g-pr-10">
                     <div class="g-pos-rel">
-                        <span class="d-block">
+                        <div class="d-block">
+                            <div class="d-inline-block g-pos-rel">
+                                300.000 {{ session('currency') == 'VEF' ? $free_currency->currency_name : session('currency') }}
+                            </div>
 
+                            <div class="d-inline-block dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    ARS
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
 
-
-                            <span class="g-pos-rel">
-                                  300.000 {{ session('currency') == 'VEF' ? $free_currency->currency_name : session('currency') }}
-                            </span>
-                            <span class="g-pos-rel"><i class="fa-regular fa-bell"></i></span>
-                            <span class="g-pos-rel"><i class="fa-solid fa-gear"></i></span>
-                           <span class="g-pos-rel">
-                               @php
-                                   $avatar = \App\Users\Users::getAvatar();
-                               @endphp
-                               @if (!is_null ($avatar))
-                                   <img class="g-width-30 img-avatar g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ $avatar }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
-                               @else
-                                   <img class="g-width-30 img-avatar g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ asset('back/img/avatar-default.jpg') }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
-                               @endif
-                            </span>
-                        </span>
-                    </div>
-                </div>
-                <div class="col-auto d-flex g-py-12 ml-auto">
-                    <div class="col-auto d-flex g-pt-5 g-pt-0--sm g-pl-5 g-pr-10">
-                        <div class="g-pos-rel">
-                            <a id="profile-menu-invoker" class="d-block" href="#!" aria-controls="profile-menu" aria-haspopup="true" aria-expanded="false" data-dropdown-event="click" data-dropdown-target="#profile-menu" data-dropdown-type="css-animation" data-dropdown-duration="300"
-                               data-dropdown-animation-in="fadeIn" data-dropdown-animation-out="fadeOut">
-                           <span class="g-pos-rel">
-                           <span class="u-badge-v2--xs u-badge--top-right g-hidden-sm-up g-bg-secondary g-mr-5"></span>
-                               @php
-                                   $avatar = \App\Users\Users::getAvatar();
-                               @endphp
-                               @if (!is_null ($avatar))
-                                   <img class="g-width-30 g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ $avatar }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
-                               @else
-                                   <img class="g-width-30 g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ asset('back/img/avatar-default.jpg') }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
-                               @endif
-                            </span>
-                                <span class="g-pos-rel">
-                                <span class="g-hidden-sm-down">
-                                    {{ isset(auth()->user()->username) ? auth()->user()->username : '' }}
-                                </span>
-                                <i class="hs-admin-angle-down g-pos-rel g-top-2 g-ml-10"></i>
-                            </span>
-                            </a>
-                            <ul id="profile-menu" class="g-pos-abs g-left-0 g-nowrap g-font-size-14 g-py-20 g-mt-30 rounded"
-                                aria-labelledby="profile-menu-invoker">
-                                <li class="mb-0">
-                                    <a class="media g-color-primary--hover g-py-5 g-px-20"
-                                       href="{{ route('auth.logout') }}">
-                                    <span class="d-flex align-self-center g-mr-12">
-                                        <i class="hs-admin-shift-right"></i>
-                                    </span>
-                                        <span class="media-body align-self-center">
-                                        {{ _i('Logout') }}
-                                    </span>
-                                    </a>
-                                </li>
-                            </ul>
+                            <div class="d-inline-block g-pos-rel"><i class="fa-regular fa-bell"></i></div>
+                            <div class="d-inline-block g-pos-rel"><i class="fa-solid fa-gear"></i></div>
+                            <div class="d-inline-block g-pos-rel">
+                                @php
+                                    $avatar = \App\Users\Users::getAvatar();
+                                @endphp
+                                @if (!is_null ($avatar))
+                                    <img class="g-width-30 img-avatar g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ $avatar }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
+                                @else
+                                    <img class="g-width-30 img-avatar g-width-40--md g-height-30 g-height-40--md rounded-circle g-mr-10--sm" src="{{ asset('back/img/avatar-default.jpg') }}" alt="{{ isset(auth()->user()->username) ? auth()->user()->username : '' }}">
+                                @endif
+                            </div>
                         </div>
                     </div>
                 </div>
