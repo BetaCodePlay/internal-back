f24ff2ec4550d25d343b1bbaf2dcf8cea2e853a7
Clandor <carlosmejias1000@gmail.com>
2023-12-20 16:53:35 -0400

    roles

--- resources/views/back/agents/role.blade.php
+++ resources/views/back/agents/role.blade.php
@@ -8,6 +8,16 @@
     <div class="wrapper-title">
         {{ _i('Role and permission management') }}
     </div>
+
+    <div class="page-role">
+        <div class="page-top">
+            <div class="search-input">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <input type="text" class="form-control" placeholder="{{ _i('Search') }}">
+            </div>
+            <button type="button" class="btn btn-theme"><i class="fa-solid fa-plus"></i> {{ _i('Create role') }}</button>
+        </div>
+    </div>
 @endsection
 
 @section('scripts')
