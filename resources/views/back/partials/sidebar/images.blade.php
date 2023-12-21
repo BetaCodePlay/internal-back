e14c16860a59a099976f353549788d57595d2992
Emilio Hernandez <emilio25informatic@gmail.com>
2023-12-21 16:25:26 -0300

    trabajando en la section de images

--- resources/views/back/partials/sidebar/images.blade.php
+++ resources/views/back/partials/sidebar/images.blade.php
@@ -1,3 +1,7 @@
+@php
+    use Dotworkers\Configurations\Enums\TemplateElementTypes;
+@endphp
+
 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
     <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden" href="javascript:void(0)"
        data-toggle="collapse" data-target="#collapseExample" aria-expanded="true">
@@ -7,10 +11,10 @@
 
         <ul id="collapseExample"
             class="u-sidebar-navigation-v1-menu u-side-nav--second-level-menu u-side-nav--second-level-menu-top mb-0 collapse show">
-            @foreach($imageSections as $image)
+            @foreach($imageSections as $sectionKey => $image)
                 <li class="u-sidebar-navigation-v1-menu-item u-side-nav--second-level-menu-item active">
                     <a class="media u-side-nav--second-level-menu-link"
-                       href="{{ route('betpay.clients.accounts.create') }}" target="_self">
+                       href="{{ route('section-images.index', [TemplateElementTypes::$home, $sectionKey]) }}" target="_self">
                         <span class="media-body align-self-center">{{ $image->text }}</span>
                         <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                     </a>
