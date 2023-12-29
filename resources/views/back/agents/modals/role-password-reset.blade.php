11e913e3563d0740a6df30a13a57f885dd8f58fd
Clandor <carlosmejias1000@gmail.com>
2023-12-28 23:59:56 -0400

    modals

--- resources/views/back/agents/modals/role-password-reset.blade.php
+++ resources/views/back/agents/modals/role-password-reset.blade.php
@@ -2,15 +2,14 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">{{ _i('Create role') }}</h5>
+                <h5 class="modal-title">{{ _i('Reset password') }}</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <p class="font-weight-bold">{{ _i('Are you sure you want to reset the agent key?') }}</p>
+                <p><span class="font-weight-bold">{{ _i('Are you sure you want to reset the agent key?') }}</span> <span>(Antonella93)</span></p>
                 <p>{{ _i('Check for security that the user is correct, when logging in the agent will be able to enter a new password.') }}</p>
-                <p><i class="fa-solid fa-arrow-right"></i> <span>Antonella93</span></p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-transparent" data-dismiss="modal">
