ce6ed9b707d3f1cf8e05548689b037f57475e6ca
Clandor <carlosmejias1000@gmail.com>
2024-01-03 15:18:33 -0400

    roles

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -1,13 +1,13 @@
 import {} from "../../commons/js/core";
 
 class Roles {
-    initTableRoles () {
+    initTableRoles() {
         /*$('#table-roles').DataTable( {
             fixedHeader: true,
             responsive: true
         });*/
 
-        $('#table-roles').DataTable( {
+        $('#table-roles').DataTable({
             ajax: 'https://dev-back.bestcasinos.lat/agents/get/direct-children?draw=2&start=0',
             processing: true,
             serverSide: true,
@@ -17,11 +17,24 @@ class Roles {
             }],
             fixedHeader: true,
             responsive: true,
-            fnCreatedRow : function(nRow,aData,iDataIndex) {
-                console.log(nRow);
-                console.log(aData);
-                console.log(iDataIndex);
-                $('td:eq(0)',nRow).html('<span class="btn-tr-details"><i class="fa-regular fa-eye"></i></span> ' + aData[0]);
+            fnCreatedRow: function (nRow, aData, iDataIndex) {
+                let options = '<div class="d-inline-block dropdown">\n' +
+                    '<button class="btn dropdown-toggle" type="button" id="dropdownRoleProfile" data-toggle="dropdown" aria-expanded="false">\n' +
+                    '    <i class="fa-solid fa-ellipsis-vertical"></i>\n' +
+                    '</button>\n' +
+                    '<ul class="dropdown-menu" aria-labelledby="dropdownRoleProfile">\n' +
+                    '   <li><a class="dropdown-item" href="#">{{ _i(\'View profile\') }}</a></li>\n' +
+                    '   <li><a class="dropdown-item" href="javascript:void(0)" data-toggle="modal" data-target="#role-create-simple">{{ _i(\'Add role\') }}</a></li>\n' +
+                    '   <li><a class="dropdown-item" href="javascript:void(0)" data-toggle="modal" data-target="#role-password-reset">{{ _i(\'Reset password\') }}</a></li>\n' +
+                    '   <li><a class="dropdown-item" href="javascript:void(0)" data-toggle="modal" data-target="#role-lock">{{ _i(\'Lock profile\') }}</a></li>\n' +
+                    '   <li><a class="dropdown-item" href="javascript:void(0)" data-toggle="modal" data-target="#role-balance">{{ _i(\'Balance adjustment\') }}</a></li>\n' +
+                    '</ul>\n' +
+                    '</div>\n' +
+                    '\n' +
+                    '<a href="#" class="btn btn-href"><i class="fa-solid fa-chevron-right"></i></a>';
+
+                $('td:eq(0)', nRow).html('<span class="btn-tr-details"><i class="fa-regular fa-eye"></i></span> ' + aData[0]);
+                $('td:eq(5)', nRow).addClass('text-right').html(options);
             }
         });
 
