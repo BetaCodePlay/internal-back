04196f1481c0eb8681808b6ca58fd935c22b6177
Clandor <carlosmejias1000@gmail.com>
2024-01-03 15:10:33 -0400

    roles

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -21,8 +21,16 @@ class Roles {
                 console.log(nRow);
                 console.log(aData);
                 console.log(iDataIndex);
+                $('td:eq(0)',nRow).html('<span class="btn-tr-details"><i class="fa-regular fa-eye"></i></span> ' + aData[0]);
             }
         });
+
+        /*"fnCreatedRow": function(nRow,aData,iDataIndex) {
+            var new_price = parseFloat(aData.price) + 0.1 * parseFloat(aData.price);
+            $('td:eq(3)',nRow).html(new_price);
+        }
+
+        $(nRow).attr('class', 'newClass');*/
 
         $('.table-load').addClass('table-complete');
         $('.page-role .loading-style').hide();
