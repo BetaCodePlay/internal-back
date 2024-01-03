5c6ae21e380e99150dc35cc913432e631553c39f
Clandor <carlosmejias1000@gmail.com>
2024-01-02 21:45:39 -0400

    role

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -10,7 +10,11 @@ class Roles {
         $('#table-roles').DataTable( {
             ajax: 'https://dev-back.bestcasinos.lat/agents/get/direct-children?draw=2&start=0',
             processing: true,
-            serverSide: true
+            serverSide: true,
+            columnDefs: [{
+                "defaultContent": "-",
+                "targets": "_all"
+            }]
         });
 
         $('.table-load').addClass('table-complete');
