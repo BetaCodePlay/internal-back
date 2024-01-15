c1038e9bb93cf061ee89ce966d1f99dd28f99d7e
Clandor <carlosmejias1000@gmail.com>
2024-01-15 19:54:34 -0400

    test

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -26,7 +26,7 @@ class Roles {
                 $('td:eq(5)', nRow).attr('data-id', aData[2]).addClass('text-right').html(buttons.html());
             },
             initComplete: function() {
-                $(this.api().table().container()).find('input').attr('autocomplete', 'new-text');
+
             },
         });
 
@@ -41,6 +41,8 @@ class Roles {
             Roles.globalusername = $username;
             Roles.globaluserid = $userid;
         });
+
+        $('input').val('');
     }
 
     userResetPassword() {
