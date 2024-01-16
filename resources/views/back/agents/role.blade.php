be2d29e6bab62ba845954d6c9eea83bcbd53fceb
Clandor <carlosmejias1000@gmail.com>
2024-01-15 20:45:39 -0400

    test

--- resources/views/back/agents/role.blade.php
+++ resources/views/back/agents/role.blade.php
@@ -105,7 +105,7 @@
                     <i class="fa-solid fa-ellipsis-vertical"></i>
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownRoleProfile">
-                    <li><a class="dropdown-item" href="#">{{ _i('View profile') }}</a></ li>
+                    <li><a class="dropdown-item roleSimple" href="#">{{ _i('View profile') }}</a></li>
                     <li><a class="dropdown-item currentDataRole" href="javascript:void(0)" data-toggle="modal" data-target="#role-create-simple">{{ _i('Add role') }}</a></li>
                     <li><a class="dropdown-item currentDataRole" href="javascript:void(0)" data-toggle="modal" data-target="#role-password-reset">{{ _i('Reset password') }}</a></li>
                     <li><a class="dropdown-item currentDataRole" href="javascript:void(0)" data-toggle="modal" data-target="#role-lock">{{ _i('Lock profile') }}</a></li>
