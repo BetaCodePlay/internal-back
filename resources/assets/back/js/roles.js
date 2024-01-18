b4a3cc633a23b347cd3c2d92741e2fe99981e59c
Clandor <carlosmejias1000@gmail.com>
2024-01-18 15:17:40 -0400

    lock

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -24,7 +24,7 @@ class Roles {
 
                 buttons.find('[data-toggle="modal"]').attr('data-userid', aData[2]).attr('data-username', aData[0]);
                 buttons.find('.roleSimple').attr('href', '/agents/role/' + aData[0]);
-                buttons.find(modalLockTarget).attr('data-value', aData[3][1]).html(aData[3][1] === true ? $(modalLockTarget).data('lock') : $(modalLockTarget).data('unlock'));
+                buttons.find(modalLockTarget).attr('data-value', aData[3][1]).html(aData[3][1] === true ? $(modalLockTarget).data('lock') : $(modalLockTarget).data('unlock')).attr('data-type', aData[3][2]);
 
                 $('td:eq(0)', nRow).html('<span class="btn-tr-details"><i class="fa-regular fa-eye"></i></span> ' + aData[0]);
                 $('td:eq(1)', nRow).html('<span class="deco-rol">' + aData[1] + '</span>');
