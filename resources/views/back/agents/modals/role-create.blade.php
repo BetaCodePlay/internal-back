9216efa076beb4f6fbfbe67bdc725f3675825bfe
Clandor <carlosmejias1000@gmail.com>
2024-01-22 17:40:29 -0400

    roles

--- resources/views/back/agents/modals/role-create.blade.php
+++ resources/views/back/agents/modals/role-create.blade.php
@@ -24,7 +24,7 @@
                                 <div class="wrap-input">
                                     <input type="text" class="form-control" name="password" id="createRolPassword">
                                     <div class="wrap-element">
-                                        <button class="btn" type="button">
+                                        <button class="btn btn-theme" type="button">
                                             <i class="fa-solid fa-arrows-rotate"></i>
                                         </button>
                                     </div>
