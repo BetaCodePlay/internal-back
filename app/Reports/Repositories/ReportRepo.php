3bd1e93a1efa7879b6639f77a5dfbe987370409e
Emilio Hernandez <emilio25informatic@gmail.com>
2024-01-24 17:42:03 -0300

    test

--- app/Reports/Repositories/ReportRepo.php
+++ app/Reports/Repositories/ReportRepo.php
@@ -28,7 +28,18 @@ class ReportRepo
             ])
             ->get();
 
+        $audits = DB::table('audits')
+            ->join('audit_types', 'audits.audit_type_id', '=', 'audit_types.id')
+            ->latest('audits.created_at')
+            ->take(10)
+            ->select([
+                'audit_types.name',
+                DB::raw("to_char(audits.created_at, 'DD Mon HH:MIAM') as formatted_date")
+            ])
+            ->get();
+
         return [
+            'audits'       => $audits,
             'transactions' => $transactions
         ];
     }
