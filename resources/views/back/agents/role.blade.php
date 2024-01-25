62a3a230e3be99e732c3c6e46bfc8c9b55ab0cae
Clandor <carlosmejias1000@gmail.com>
2024-01-25 10:58:59 -0400

    roles

--- resources/views/back/agents/role.blade.php
+++ resources/views/back/agents/role.blade.php
@@ -51,12 +51,12 @@
                             </div>
                             <div class="tab-manager-data">
                                 <div class="data-title">{{ _i('ID User') }}</div>
-                                <div class="data-text text-id">{{ $authUser->id}}</div>
+                                <div class="data-text text-id">{{ $authUser->id}} <button class="btn btn-theme btn-xs">{{ _i('Copy') }}</button></div>
                             </div>
                             <div class="tab-manager-data">
                                 <div class="data-title">{{ _i('Password') }}</div>
                                 <div class="data-text">
-                                    <button class="btn btn-theme btn-reset-password-head" data-toggle="modal" data-target="#role-password-reset" data-id="{{ $authUser->id}}">{{ _i('Reset') }}</button>
+                                    <button class="btn btn-theme btn-xs btn-reset-password-head" data-toggle="modal" data-target="#role-password-reset" data-id="{{ $authUser->id}}">{{ _i('Reset') }}</button>
                                 </div>
                             </div>
                         </div>
