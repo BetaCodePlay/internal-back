9e6050448f2c35abbc04b83e08ab3d77efcd2838
Clandor <carlosmejias1000@gmail.com>
2024-01-30 16:12:09 -0400

    copy

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -476,10 +476,6 @@ class Roles {
         }
     };
 
-    userTabTransaction(route) {
-
-    }
-
     static errorResponse(json) {
         let array = Object.values(json.responseJSON.errors);
         let title = json.responseJSON.message;
