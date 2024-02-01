d8c1af51d8e74587a726d6536e401278733dfdd2
Emilio Hernandez <emilio25informatic@gmail.com>
2024-02-01 19:53:23 -0300

    test

--- app/Audits/Repositories/AuditsRepo.php
+++ app/Audits/Repositories/AuditsRepo.php
@@ -133,8 +133,13 @@ class AuditsRepo
             ->get();
 
         $resultCount   = $audit->count();
-        $slicedResults = $audit->slice($start, $length)->all();
-
+        $slicedResults = $audit->slice($start, $length)->map(function ($item) {
+            return [
+                $item['data']['ip'],
+                $item['quantity'],
+            ];
+        })->all();
+        
         return [
             'draw'            => (int)$draw,
             'recordsTotal'    => $resultCount,
