29fcf50d3878f57a8597606870df651966c56bb8
Clandor <carlosmejias1000@gmail.com>
2024-02-11 14:56:50 -0400

    roles

--- resources/views/back/agents/role-dashboard.blade.php
+++ resources/views/back/agents/role-dashboard.blade.php
@@ -5,5 +5,17 @@
 @endsection
 
 @section('content')
+    <div class="wrapper-title g-pb-30">
+        {{ _i('Dashboard') }}
+    </div>
 
+    <div class="page-dashboard">
+        <div class="row">
+            <div class="col-12 col-lg-4">
+                <div class="dashboard-content">
+                    <div class="dashboard-content-title">{{ _i('Total balance') }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
