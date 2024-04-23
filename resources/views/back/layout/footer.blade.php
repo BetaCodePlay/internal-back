4ea2695f101b973e92efbf2c3e8699b2b3d1560a
Clandor <carlosmejias1000@gmail.com>
2024-04-23 19:00:45 -0400

    col general

--- resources/views/back/layout/footer.blade.php
+++ resources/views/back/layout/footer.blade.php
@@ -1,14 +1,16 @@
 <footer class="footer">
-    <div class="footer-top">
-        <div class="footer-top-left">
-            @if(!empty($logo))
-                @if(!is_null($logo->img_dark))
-                    <img src="{{$logo->img_dark }}" alt="Logo" width="180" height="37" class="img-logo">
+    <div class="footer-ex">
+        <div class="footer-top">
+            <div class="footer-top-left">
+                @if(!empty($logo))
+                    @if(!is_null($logo->img_dark))
+                        <img src="{{$logo->img_dark }}" alt="Logo" width="180" height="37" class="img-logo-footer">
+                    @endif
                 @endif
-            @endif
-        </div>
-        <div class="footer-top-right">
-            <a href="#">{{ _i('Help') }}</a>
+            </div>
+            <div class="footer-top-right">
+                <a href="#">{{ _i('Help') }}</a>
+            </div>
         </div>
     </div>
 </footer>
