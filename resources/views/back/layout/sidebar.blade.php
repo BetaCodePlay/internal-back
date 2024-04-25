6c1b553fc59a3dbc77bce021a181894328071dad
Clandor <carlosmejias1000@gmail.com>
2024-04-25 16:51:02 -0400

    col general

--- resources/views/back/layout/sidebar.blade.php
+++ resources/views/back/layout/sidebar.blade.php
@@ -17,32 +17,30 @@
             </div>
         </li>
 
+        @can('access', [\Dotworkers\Security\Enums\Permissions::$users_search])
+            <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
+                <div class="u-sidebar-title"><span>{{ _i('Search global') }}</span></div>
+            </li>
+            <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
+                <form id="header-search-form" class="u-header--search" action="{{ route('users.search') }}" method="get" autocomplete="destroy">
+                    <div class="input-group">
+                        <input class="form-control form-control-sidebar" type="text" name="username" placeholder="{{ _i('Search user') }}" value="">
+                        <button type="submit" class="btn btn-search u-btn-outline-white g-brd-none g-bg-transparent--hover g-pos-abs g-top-0 g-right-0 d-flex g-width-40 h-100 align-items-center justify-content-center g-font-size-18 g-z-index-2">
+                            <i class="hs-admin-search"></i>
+                        </button>
+                    </div>
+                </form>
+            </li>
+            <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active u-sidebar-navigation-v1-menu-item-search">
+                <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden">
+                    <span class="g-pos-rel"><i class="fa-solid fa-magnifying-glass"></i></span>
+                </a>
+            </li>
 
-        <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <div class="u-sidebar-title"><span>{{ _i('Search global') }}</span></div>
-        </li>
-        <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <form id="header-search-form" class="u-header--search" action="{{ route('users.search') }}" method="get" autocomplete="destroy">
-                <div class="input-group">
-                    <input class="form-control form-control-sidebar" type="text" name="username" placeholder="{{ _i('Search user') }}" value="">
-                    <button type="submit" class="btn btn-search u-btn-outline-white g-brd-none g-bg-transparent--hover g-pos-abs g-top-0 g-right-0 d-flex g-width-40 h-100 align-items-center justify-content-center g-font-size-18 g-z-index-2">
-                        <i class="hs-admin-search"></i>
-                    </button>
-                </div>
-            </form>
-        </li>
-        <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active u-sidebar-navigation-v1-menu-item-search">
-            <a class="media u-side-nav--top-level-menu-link u-side-nav--hide-on-hidden">
-                <span class="g-pos-rel"><i class="fa-solid fa-magnifying-glass"></i></span>
-            </a>
-        </li>
-
-        <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
-            <hr>
-        </li>
-
-        {{-- @can('access', [\Dotworkers\Security\Enums\Permissions::$users_search])--}}
-        {{--@endcan--}}
+            <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
+                <hr>
+            </li>
+        @endcan
 
         <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
             <div class="u-sidebar-title"><span>{{ _i('Categories') }}</span></div>
@@ -237,6 +235,7 @@
 
 
 
+
 @endif
 @can('access', [$permissions::$agents_dashboard])
     <li class="u-sidebar-navigation-v1-menu-item u-side-nav--top-level-menu-item has-active">
@@ -245,6 +244,7 @@
                     <span class="icon-mobile"><i class="fa-solid fa-chevron-down"></i></span>
                 </a>
             </li>
+
 
 
 
@@ -364,9 +364,11 @@
 
 
 
+
 @endif
 @can('access', [$permissions::$agents_dashboard])
     <div class="nav-mobile-opt"><a href="{{ route('agents.index') }}"><i class="fa-solid fa-people-group"></i> <span class="name">{{ _i('Role') }}</span></a></div>
+
 
 
 
