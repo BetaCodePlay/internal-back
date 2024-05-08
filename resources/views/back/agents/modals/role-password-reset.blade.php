e5e147cceb7caf3a13710c759515476d501d9091
Clandor <carlosmejias1000@gmail.com>
2024-05-08 15:11:10 -0400

    roles

--- resources/views/back/agents/modals/role-password-reset.blade.php
+++ resources/views/back/agents/modals/role-password-reset.blade.php
@@ -10,7 +10,7 @@
             <div class="modal-body">
                 <p><span class="font-weight-bold text-form">{{ _i('Are you sure you want to reset the agent key?') }}</span> <span class="username-form"></span></p>
                 <p>{{ _i('Check for security that the user is correct, when logging in the agent will be able to enter a new password.') }}</p>
-                <div class="form">
+                <form class="form" autocomplete="destroy">
                     <div class="row">
                         <div class="col-12">
                             <div class="form-group">
@@ -24,7 +24,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-transparent" data-dismiss="modal">
