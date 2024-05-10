d125843d71a3289269a99e6d18b74c00900b3111
Hember Colmenares <hemberfer@gmail.com>
2024-05-10 19:04:13 +0200

    Fix paginator in transactions

--- resources/js/vue/components/reports/tables/transactions.vue
+++ resources/js/vue/components/reports/tables/transactions.vue
@@ -14,12 +14,6 @@
             class="mt-3"
             :value="items"
             responsiveLayout="scroll"
-            paginator
-            :totalRecords="totalRecords"
-            :rows="perPage"
-            :currentPage="currentPage"
-            :onPage="onPageChange"
-            :rowsPerPageOptions="[10, 20, 50]"
         >
             <Column
                 v-for="col of columns"
@@ -30,14 +24,14 @@
                 <template #body="slotProps">
                     <div class="text-center" v-if="col.field == 'date'">
                         <strong>{{
-                                moment(slotProps.data.date).format("YYYY-MM-DD")
-                            }}</strong>
+                            slotProps.data.date
+                        }}</strong>
                     </div>
                     <div
                         class="text-center"
                         v-else-if="col.field == 'new_amount'"
                     >
-                        <div v-html="slotProps.data.new_amount"></div>
+                        {{ slotProps.data.new_amount }}
                     </div>
                     <div class="text-center" v-else-if="col.field == 'from'">
                         {{ slotProps.data.data.from }}
@@ -55,20 +49,14 @@
             <div class="loading-style" v-if="loading"></div>
         </DataTable>
 
-        <template>
-            <div class="card">
-                <Paginator :rows="perPage" :totalRecords="totalRecords" :rowsPerPageOptions="[10, 20, 30]">
-                    <template #start="slotProps">
-                        Page: {{ slotProps.state.page }}
-                        First: {{ slotProps.state.first }}
-                        Rows: {{ slotProps.state.rows }}
-                    </template>
-                    <template #end>
-                        <Button type="button" icon="pi pi-search" />
-                    </template>
-                </Paginator>
-            </div>
-        </template>
+        <Paginator
+            :rows="perPage"
+            :totalRecords="totalRecords"
+            :rowsPerPageOptions="[10, 20, 30]"
+            @page="onPageChange"
+        >
+           
+        </Paginator>
     </div>
 </template>
 <script>
@@ -98,11 +86,11 @@ export default {
             },
             items: [],
             columns: [
-                {field: "date", header: "Fecha"},
-                {field: "from", header: "Agente"},
-                {field: "to", header: "Cuenta destino"},
-                {field: "new_amount", header: "Monto"},
-                {field: "balance", header: "Balance"},
+                { field: "date", header: "Fecha" },
+                { field: "from", header: "Agente" },
+                { field: "to", header: "Cuenta destino" },
+                { field: "new_amount", header: "Monto" },
+                { field: "balance", header: "Balance" },
             ],
         };
     },
@@ -136,7 +124,6 @@ export default {
             XLSX.writeFile(wb, filename);
         },
         exportData(type) {
-            console.log(type);
             switch (type) {
                 case "excel":
                     this.exportXLS();
@@ -148,11 +135,13 @@ export default {
         },
         onPageChange(event) {
             this.currentPage = event.page + 1;
+            this.perPage = event.rows;
             this.fetchData();
         },
         async fetchData() {
             this.loading = true;
-            const {daterange, typeUser, typeTransaction, selectedTimezone} = this.filters;
+            const { query, daterange, typeUser, typeTransaction, selectedTimezone } =
+                this.filters;
 
             if (daterange[1]) {
                 const startDate = moment(daterange[0]).format("YYYY-MM-DD");
@@ -160,30 +149,31 @@ export default {
                 const userId = window.authUserId;
 
                 try {
-                    const {data} = await axios.get(`/agents/${userId}/transactions`, {
-                        params: {
-                            startDate,
-                            endDate,
-                            typeUser,
-                            typeTransaction,
-                            timezone: selectedTimezone,
-                            per_page: this.perPage,
-                            page: this.currentPage
+                    const { data } = await axios.get(
+                        `/agents/${userId}/transactions`,
+                        {
+                            params: {
+                                startDate,
+                                endDate,
+                                typeUser,
+                                query,
+                                typeTransaction,
+                                timezone: selectedTimezone,
+                                per_page: this.perPage,
+                                page: this.currentPage,
+                            },
                         }
-                    });
+                    );
 
                     this.items = data.data;
                     this.totalRecords = data.total;
-
-                    console.log(data, data.total, data.per_page);
                 } catch (error) {
                     console.error(error);
                 } finally {
                     this.loading = false;
                 }
             }
-        }
-
+        },
     },
     mounted() {
         this.fetchData();
