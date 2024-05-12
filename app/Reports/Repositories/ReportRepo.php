fc9c71fd9b57c15e59017a0cb1a6bf5d9b1f2f4c
Emilio Hernandez <emilio25informatic@gmail.com>
2024-05-12 16:35:52 -0400

    role-dashboard.blade.php

--- app/Reports/Repositories/ReportRepo.php
+++ app/Reports/Repositories/ReportRepo.php
@@ -86,6 +86,9 @@ class ReportRepo
             'field'        => 'played',
         ]);
 
+
+        dd($this->gamesRepo->bestMakers($whitelabelId, $currency, $lastMonth));
+
         return [
             'audits'       => $audits,
             'amounts'      => [
