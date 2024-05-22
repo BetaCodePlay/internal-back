efac037b04bc525a734dd629996fbdb77babcb8d
Clandor <carlosmejias1000@gmail.com>
2024-05-22 11:30:57 -0400

    balance

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -247,13 +247,13 @@ class Roles {
                         let {wallet} = data;
                         resolve(wallet);
 
-                        if (Roles.globaluserid === Roles.globaluseridcurrent) {
+                        /*if (Roles.globaluserid === Roles.globaluseridcurrent) {
                             window.location.reload()
                         } else {
                             if (Roles.globaltable !== undefined) {
                                 Roles.globaltable.ajax.reload();
                             }
-                        }
+                        }*/
                     },
                     error: function (error) {
                         console.error("Error obtaining user information:", error);
