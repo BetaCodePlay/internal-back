9f0bab069bdcee4fabbbeb02485f4e9f5373969e
Clandor <carlosmejias1000@gmail.com>
2024-05-29 22:54:18 -0400

    datatable

--- resources/assets/back/js/roles.js
+++ resources/assets/back/js/roles.js
@@ -753,7 +753,7 @@ class Roles {
     };
 
     static inputMoney($input, $post) {
-        function formatMoney(number, places, symbol, thousand, decimal) {
+        /*function formatMoney(number, places, symbol, thousand, decimal) {
             places = !isNaN(places = Math.abs(places)) ? places : 2;
             symbol = symbol !== undefined ? symbol : "";
             thousand = thousand || ",";
@@ -762,6 +762,17 @@ class Roles {
                 i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
                 j = (j = i.length) > 3 ? j % 3 : 0;
             return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand);
+        }*/
+
+        function formatMoney(number, places, symbol, thousand, decimal) {
+            places = !isNaN(places = Math.abs(places)) ? places : 2;
+            symbol = symbol !== undefined ? symbol : "";
+            thousand = thousand || ",";
+            decimal = decimal || ".";
+            var negative = number < 0 ? "-" : "",
+                i = parseInt(number = Math.abs(+number || 0).toFixed(places), 10) + "",
+                j = (j = i.length) > 3 ? j % 3 : 0;
+            return symbol + negative + (j ? i.substr(0, j) + thousand : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousand) + (places ? decimal + Math.abs(number - i).toFixed(places).slice(2) : "");
         }
 
         $(document).on('input', $input, function () {
