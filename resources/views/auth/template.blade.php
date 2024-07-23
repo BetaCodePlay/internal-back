73a34084d89687a82b4358922d4a7e66dd7a4087
Clandor <carlosmejias1000@gmail.com>
2024-07-23 19:21:02 -0400

    theme

--- resources/views/auth/template.blade.php
+++ resources/views/auth/template.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
     <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
-    <link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.011">
+    <link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.012">
     <!--===============================================================================================-->
 </head>
 <body class="body-auth">
