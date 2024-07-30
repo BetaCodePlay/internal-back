eec1141f8a4b0f97430bf1ae4a49d9d4ffde1a98
Clandor <carlosmejias1000@gmail.com>
2024-07-30 12:34:12 -0400

    bet30

--- resources/views/auth/template.blade.php
+++ resources/views/auth/template.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
     <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
     <!--<link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.020">-->
-    <link rel="stylesheet" href="{{ asset("themes/fortubet/theme.min.css") }}?v=1.021">
+    <link rel="stylesheet" href="{{ asset("themes/fortubet/theme.min.css") }}?v=1.022">
     <!--===============================================================================================-->
 </head>
 <body class="body-auth">
