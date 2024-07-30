5f0a987102d5160f3d363cb646b51d1af3723cb9
Clandor <carlosmejias1000@gmail.com>
2024-07-30 09:36:25 -0400

    bet30

--- resources/views/auth/template.blade.php
+++ resources/views/auth/template.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="{{ mix('css/vendor.min.css', 'auth') }} ">
     <link rel="stylesheet" href="{{ mix('css/custom.min.css', 'auth') }} ">
     <!--<link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.020">-->
-    <link rel="stylesheet" href="{{ asset("themes/ganaencasa/theme.min.css") }}?v=1.021">
+    <link rel="stylesheet" href="{{ asset("themes/bet30/theme.min.css") }}?v=1.021">
     <!--===============================================================================================-->
 </head>
 <body class="body-auth">
