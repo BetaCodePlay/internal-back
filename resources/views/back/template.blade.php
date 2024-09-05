42b2e0cce94106c392808440204d81680c775800
Clandor <carlosmejias1000@gmail.com>
2024-09-05 19:15:23 -0400

    admin

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -19,7 +19,7 @@
     <link rel="stylesheet" href="{{ asset('back/css/vendor.min.css') }}?v=6.33">
     <link rel="stylesheet" href="{{ asset('back/css/custom.min.css') }}?v=12.43">
     <link href="https://unpkg.com/primeicons/primeicons.css " rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.018">
+    <link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.019">
     <!--    <link rel="stylesheet" href="{{ asset("themes/space/theme.min.css") }}?v=1.034">-->
 
     <!--<link href="https://cdn.datatables.net/2.0.0/css/dataTables.dataTables.css" rel="stylesheet">-->
