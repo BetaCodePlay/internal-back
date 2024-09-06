139b6f33703b54e5fbfd00d42c6ed9eb21a9c0a2
Emilio Hernandez <emilio25informatic@gmail.com>
2024-09-06 15:58:22 -0400

    test

--- app/Http/Controllers/AgentsController.php
+++ app/Http/Controllers/AgentsController.php
@@ -748,6 +748,7 @@ class AgentsController extends Controller
     public function transactions(Request $request, string|int $agent)
     : JsonResponse|Response {
         try {
+            dd('here');
             $transactions = $this->transactionsRepo->getUserProviderTransactionsPaginated($request, $agent);
 
             return response()->json(
