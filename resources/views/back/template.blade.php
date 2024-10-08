b31fd15f24f30df8184a857b107cb33f005aeb72
Clandor <carlosmejias1000@gmail.com>
2024-10-08 15:14:31 -0400

    unicornio

--- resources/views/back/template.blade.php
+++ resources/views/back/template.blade.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="{{ asset('back/css/custom.min.css') }}?v=12.43">
     <link href="https://unpkg.com/primeicons/primeicons.css " rel="stylesheet">
     {{--<link rel="stylesheet" href="{{ asset("themes/$theme") }}?v=1.021">--}}
-    <link rel="stylesheet" href="{{ asset("themes/unicornio/theme.min.css") }}?v=1.034">
+    <link rel="stylesheet" href="{{ asset("themes/unicornio/theme.min.css") }}?v=1.035">
 
     <!--<link href="https://cdn.datatables.net/2.0.0/css/dataTables.dataTables.css" rel="stylesheet">-->
 
