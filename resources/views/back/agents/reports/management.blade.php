fe488e01a663f25f900261bd640264d667214f31
Clandor <carlosmejias1000@gmail.com>
2024-10-08 16:40:15 -0400

    unicornio

--- resources/views/back/agents/reports/management.blade.php
+++ resources/views/back/agents/reports/management.blade.php
@@ -1,6 +1,8 @@
 @extends('back.template')
 @section('content')
-    <pages-reports />
+    <div class="page-reports">
+        <pages-reports />
+    </div>
 @endsection
 @section('styles')
     <link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
