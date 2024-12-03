2e2e65c49f54c83ceaceb5b4d0032e5192c91e4f
Genesis Perez <genesis@nannoxtech.com>
2024-12-03 16:09:43 -0400

    log

--- routes/web/financial-report.php
+++ routes/web/financial-report.php
@@ -70,7 +70,7 @@ Route::group(['prefix' => 'financial-report', 'middleware' => ['auth']], functio
         ]);
 
         // Store financial report
-        Route::post('search', [
+        Route::get('search', [
             'as' => 'financial-report.providers.search',
             'uses' => 'FinancialReportController@search'
         ]);
