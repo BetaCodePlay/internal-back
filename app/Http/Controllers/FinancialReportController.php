5d5fc9d0b574a47d12e0fe5bae7da658bb6be1b1
Genesis Perez <genesis@nannoxtech.com>
2024-12-10 14:06:42 -0400

    provider

--- app/Http/Controllers/FinancialReportController.php
+++ app/Http/Controllers/FinancialReportController.php
@@ -267,7 +267,7 @@ class FinancialReportController
                 $percentage = $request->percentage;
                 $chips = $request->chips;
                 $report = $this->financialReportRepo->reportBenefit($provider, $maker, $currency, $startDate, $endDate, $timezone, $percentage, $chips);
-                $this->financialReportCollection->formatAllReportProvider($report, $provider, $maker, $startDate, $percentage, $chips);
+                $this->financialReportCollection->formatAllReportProvider($report, $request->change_provider, $maker, $startDate, $percentage, $chips);
             } else {
                 $report = [];
             }
